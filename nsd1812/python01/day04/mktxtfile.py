6d503c70f0c2884eef6f869860dc57da847357c3
MrZhangzhg <zhangzg@tedu.cn>
2019-05-10 15:33:36 +0800

    py 1812 python01 day04 15:30

---
+++ nsd1812/python01/day04/mktxtfile.py
@@ -0,0 +1,13 @@
+def get_fname():
+
+
+def get_content():
+
+
+def wfile(fname, content):
+
+
+if __name__ == '__main__':
+    fname = get_fname()
+    content = get_content()
+    wfile(fname, content)
