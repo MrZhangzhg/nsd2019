4ac074fccf88304c247542f220e70c96698c3f3a
MrZhangzhg <zhangzg@tedu.cn>
2019-05-11 14:21:22 +0800

    py 1812 python01 day05 14:20

--- nsd1812/python01/day05/stack.py
+++ nsd1812/python01/day05/stack.py
@@ -1,14 +1,35 @@
 def push_it():
-
+    print('push')
 
 def pop_it():
+    print('pop')
 
 
 def view_it():
-
+    print('view')
 
 def show_menu():
+    prompt = """(0) 压栈
+(1) 出栈
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
 
+    while True:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2', '3']:
+            print('无效的输入，请重试。')
+            continue
+
+        if choice == '0':
+            push_it()
+        elif choice == '1':
+            pop_it()
+        elif choice == '2':
+            view_it()
+        else:
+            print('Bye-bye')
+            break
 
 if __name__ == '__main__':
     show_menu()
