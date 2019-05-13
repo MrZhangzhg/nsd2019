16d6a21818409adfaa581af946b6eb672df69748
MrZhangzhg <zhangzg@tedu.cn>
2019-05-13 14:47:00 +0800

    py 1812 python02 day01 14:50

---
+++ nsd1812/python02/day01/trigger_err.py
@@ -0,0 +1,12 @@
+def set_age(name, age):
+    if not 0 < age < 120:
+        raise ValueError('年龄超出范围')
+
+    print('%s is %d years old' % (name, age))
+
+if __name__ == '__main__':
+    # set_age('崔军', 244)
+    try:
+        set_age('崔军', 244)
+    except ValueError as e:
+        print('Error:', e)
