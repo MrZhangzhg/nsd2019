ec80d8d70d8d7ae9643dc3223a8727dc6aaf0399
MrZhangzhg <zhangzg@tedu.cn>
2019-05-13 11:57:58 +0800

    py 1812 python02 day01 11:58

--- nsd1812/python02/day01/myerr.py
+++ nsd1812/python02/day01/myerr.py
@@ -4,5 +4,15 @@ try:
     print(result)
 except ValueError:
     print('无效输入')
+    exit(1)
+except ZeroDivisionError:
+    print('无效输入')
+    exit(1)
+except KeyboardInterrupt:
+    print('\nBye-bye')
+    exit(2)
+except EOFError:
+    print('\nBye-bye')
+    exit(2)
 
 print('Done')
