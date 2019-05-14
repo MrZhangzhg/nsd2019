5a84731c4e2a46db858eb1d0add11b5aa5653be4
MrZhangzhg <zhangzg@tedu.cn>
2019-05-14 14:25:43 +0800

    py 1812 python02 day02 14:30

--- nsd1812/python02/day02/math_game.py
+++ nsd1812/python02/day02/math_game.py
@@ -1,7 +1,7 @@
 from random import randint, choice
 
 def exam():
-    nums = [randint(1, 100) for i in range(2)]
+    nums = [randint(1, 100) for i in range(2)]  # 生成两个数字
     nums.sort(reverse=True)  # 降序排列
     op = choice('+-')
     if op == '+':
@@ -9,13 +9,19 @@ def exam():
     else:
         result = nums[0] - nums[1]
 
-    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
-    answer = int(input(prompt))
-    if answer == result:
-        print('Very Good!!!')
-    else:
-        print('Wrong Answer!!!')
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])  # 5 + 3 =
+    counter = 0
 
+    while counter < 3:
+        answer = int(input(prompt))
+        if answer == result:
+            print('Very Good!!!')
+            break
+
+        print('Wrong Answer!!!')
+        counter += 1
+    else:  # 三次全答错，才会输出正确答案
+        print('%s%s' % (prompt, result))
 
 if __name__ == '__main__':
     while True:
