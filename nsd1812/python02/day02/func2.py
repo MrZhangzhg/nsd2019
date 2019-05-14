7d9f91bc8667f1b9288b51ea345db73b23a33b84
MrZhangzhg <zhangzg@tedu.cn>
2019-05-14 16:52:22 +0800

    py 1812 python02 day02 16:50

---
+++ nsd1812/python02/day02/func2.py
@@ -0,0 +1,13 @@
+def func(n):
+    if n == 1:
+        return 1
+
+    return n * func(n - 1)
+        #  5 * fnuc(4)
+        #  5 * 4 * func(3)
+        #  5 * 4 * 3 * func(2)
+        #  5 * 4 * 3 * 2 * func(1)
+        #  5 * 4 * 3 * 2 * 1
+
+if __name__ == '__main__':
+    print(func(5))
