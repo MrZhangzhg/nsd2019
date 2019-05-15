35749300235debb3fa578812290603b6df0312ef
MrZhangzhg <zhangzg@tedu.cn>
2019-05-15 16:51:08 +0800

    py 1812 python02 day03 16:50

--- nsd1812/python02/day03/myclass.py
+++ nsd1812/python02/day03/myclass.py
@@ -2,14 +2,23 @@ class A:
     def foo(self):
         print('a foo')
 
+    def fn1(self):
+        print('a fn')
+
 class B:
     def bar(self):
         print('b bar')
 
-class C(A, B):
+    def fn1(self):
+        print('b fn')
+
+class C(B, A):
     pass
+    # def fn1(self):
+    #     print('c fn')
 
 if __name__ == '__main__':
     c1 = C()
     c1.foo()
     c1.bar()
+    c1.fn1()
