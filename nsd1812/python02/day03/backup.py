e1bf1c970cce84590bad137f2b2dcd9a8379aa9a
MrZhangzhg <zhangzg@tedu.cn>
2019-05-15 14:37:43 +0800

    py 1812 python02 day03 14:40

--- nsd1812/python02/day03/backup.py
+++ nsd1812/python02/day03/backup.py
@@ -39,13 +39,38 @@ def full_backup(src_dir, dst_dir, md5file):
         pickle.dump(md5dict, fobj)
 
 def incr_backup(src_dir, dst_dir, md5file):
-    pass
+    # 拼接出目标文件的绝对路径
+    fname = os.path.basename(src_dir)
+    fname = '%s_incr_%s.tar.gz' % (fname, strftime('%Y%m%d'))
+    fname = os.path.join(dst_dir, fname)
+
+    # 计算每个文件的md5值，保存到字典中
+    md5dict = {}
+    for path, folders, files in os.walk(src_dir):
+        for file in files:
+            key = os.path.join(path, file)
+            md5dict[key] = check_md5(key)
+
+    # 取出前一天的md5值
+    with open(md5file, 'rb') as fobj:
+        old_md5 = pickle.load(fobj)
+
+    # 备份新增的文件和已改动的文件
+    tar = tarfile.open(fname, 'w:gz')
+    for key in md5dict:
+        if old_md5.get(key) != md5dict[key]:
+            tar.add(key)
+    tar.close()
+
+    # 更新md5文件
+    with open(md5file, 'wb') as fobj:
+        pickle.dump(md5dict, fobj)
 
 if __name__ == '__main__':
     src_dir = '/tmp/demo/security'
     dst_dir = '/tmp/demo'
     md5file = '/tmp/demo/md5.data'
-    if strftime('%a') != 'Mon':
+    if strftime('%a') == 'Mon':
         full_backup(src_dir, dst_dir, md5file)
     else:
         incr_backup(src_dir, dst_dir, md5file)
