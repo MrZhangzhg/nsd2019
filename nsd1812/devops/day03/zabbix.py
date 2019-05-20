2b4e5dc6d7696838ab2275cd77c2177e4cd722c0
MrZhangzhg <zhangzg@tedu.cn>
2019-05-20 10:46:39 +0800

    1812 devops day03 10:50

--- nsd1812/devops/day03/zabbix.py
+++ nsd1812/devops/day03/zabbix.py
@@ -13,16 +13,115 @@ headers = {'Content-Type': 'application/json-rpc'}
 # }
 ###################################
 # 获取令牌
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "user.login",
+#     "params": {
+#         "user": "Admin",
+#         "password": "zabbix"
+#     },
+#     "id": 1
+# }
+# 99845118dd0855dcc0eafdb94d6175ca
+###################################
+# 获取主机信息
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "host.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             # "host": [
+#             #     "Zabbix server",
+#             #     "Linux server"
+#             # ]
+#         }
+#     },
+#     "auth": "99845118dd0855dcc0eafdb94d6175ca",
+#     "id": 1
+# }
+###################################
+# 删除主机
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "host.delete",
+#     "params": [
+#         "10255",
+#     ],
+#     "auth": "99845118dd0855dcc0eafdb94d6175ca",
+#     "id": 1
+# }
+###################################
+# 取出Linux Servers组号
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "99845118dd0855dcc0eafdb94d6175ca",
+#     "id": 1
+# }
+# groupid: 2
+###################################
+# 取出Template OS Linux模板的ID
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#             ]
+#         }
+#     },
+#     "auth": "99845118dd0855dcc0eafdb94d6175ca",
+#     "id": 1
+# }
+# 'templateid': '10001'
+###################################
+# 创建主机
 data = {
     "jsonrpc": "2.0",
-    "method": "user.login",
+    "method": "host.create",
     "params": {
-        "user": "Admin",
-        "password": "zabbix"
+        "host": "nsd1812web1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.4.2",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "123456789012",
+            "macaddress_b": "fsdalkjfsdaljfdsa"
+        }
     },
+    "auth": "99845118dd0855dcc0eafdb94d6175ca",
     "id": 1
 }
-# 99845118dd0855dcc0eafdb94d6175ca
+
 
 
 ###################################
