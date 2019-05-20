b32b5901bbb2ed0874674e071735460a24b04de4
MrZhangzhg <zhangzg@tedu.cn>
2019-05-20 16:56:34 +0800

    1812 devops day03 16:55

---
+++ nsd1812/devops/day03/runpb.py
@@ -0,0 +1,68 @@
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.executor.playbook_executor import PlaybookExecutor
+
+Options = namedtuple(
+    'Options',
+    [
+        'connection',
+        'remote_user',
+        'ask_sudo_pass',
+        'verbosity',
+        'ask_pass',
+        'module_path',
+        'forks',
+        'become',
+        'become_method',
+        'become_user',
+        'check',
+        'listhosts',
+        'listtasks',
+        'listtags',
+        'syntax',
+        'sudo_user',
+        'sudo',
+        'diff'
+    ]
+)
+options = Options(
+    connection='smart',
+    remote_user=None,
+    ask_pass=None,
+    sudo_user=None,
+    forks=5,
+    sudo=None,
+    ask_sudo_pass=False,
+    verbosity=5,
+    module_path=None,
+    become=None,
+    become_method=None,
+    become_user=None,
+    check=False,
+    diff=False,
+    listhosts=None,
+    listtasks=None,
+    listtags=None,
+    syntax=None
+)
+loader = DataLoader()
+passwords = dict()
+inventory = InventoryManager(loader=loader, sources=['myansible/hosts'])
+variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+def runpb(pb_path):
+    playbook = PlaybookExecutor(
+        playbooks=pb_path,
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=loader,
+        options=options,
+        passwords=passwords
+    )
+    result = playbook.run()
+    return result
+
+if __name__ == '__main__':
+    runpb(pb_path=['myansible/lamp.yml'])
