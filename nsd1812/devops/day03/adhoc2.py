d4e2934c987990f5b4454306a876eded12b1599a
MrZhangzhg <zhangzg@tedu.cn>
2019-05-20 16:44:50 +0800

    1812 devops day03 16:50

---
+++ nsd1812/devops/day03/adhoc2.py
@@ -0,0 +1,46 @@
+import shutil
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+import ansible.constants as C
+
+def adhoc(sources=None, hosts=None, module=None, args=None):
+    Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+    options = Options(connection='ssh', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+    loader = DataLoader()
+    passwords = dict()
+    inventory = InventoryManager(loader=loader, sources=sources)
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    play_source = dict(
+            name="Ansible Play",  # play的名字
+            hosts=hosts,    # 指定在哪些主机上执行命令
+            gather_facts='no',
+            tasks=[
+                dict(action=dict(module=module, args=args), register='shell_out'),
+             ]
+        )
+    play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+    tqm = None
+    try:
+        tqm = TaskQueueManager(
+                  inventory=inventory,
+                  variable_manager=variable_manager,
+                  loader=loader,
+                  options=options,
+                  passwords=passwords,
+              )
+        result = tqm.run(play)
+    finally:
+        if tqm is not None:
+            tqm.cleanup()
+
+        shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
+if __name__ == '__main__':
+    sources = ['myansible/hosts']
+    hosts = 'webservers'
+    module = 'shell'
+    args = 'useradd alice'
+    adhoc(sources, hosts, module, args)
