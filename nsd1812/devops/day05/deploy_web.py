821a52f8ce155c139d652cb48e578fab5d85d5dd
MrZhangzhg <zhangzg@tedu.cn>
2019-05-22 17:07:34 +0800

    1812 devops day05 17:11

--- nsd1812/devops/day05/deploy_web.py
+++ nsd1812/devops/day05/deploy_web.py
@@ -1,15 +1,57 @@
 import requests
 import wget
 import os
+import hashlib
+import tarfile
 
 def has_new_ver(ver_url, ver_fname):
+    # 如果本地没有版本文件，则有新版本
+    if not os.path.isfile(ver_fname):
+        return True
 
+    # 取出本地版本
+    with open(ver_fname) as fobj:
+        local_ver = fobj.read()
+
+    # 如果本地版本和远程版本不一致，则有新版本
+    r = requests.get(ver_url)
+    if local_ver != r.text:
+        return True
+
+    return False
 
 def check_pkgs(md5_url, pkg_path):
+    # 取出远程md5值
+    r = requests.get(md5_url)
+    remote_md5 = r.text.strip()
 
+    m = hashlib.md5()
+    with open(pkg_path, 'rb') as fobj:
+        while True:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+    local_md5 = m.hexdigest()
 
-def deploy(pkg_path, deploy_dir):
+    if local_md5 == remote_md5:
+        return True
 
+    return False
+
+def deploy(pkg_path, deploy_dir, version):
+    dest = '/var/www/html/nsd1812'
+    src = '/var/www/deploy/myweb-%s' % version
+    tar = tarfile.open(pkg_path, 'r')
+    tar.extractall(path=deploy_dir)
+    tar.close()
+
+    # 如果目标链接已存在，先删除，否则无法创建
+    if os.path.exists(dest):
+        os.remove(dest)
+
+    # 创建链接
+    os.symlink(src, dest)
 
 if __name__ == '__main__':
     # 如果没有新版本则退出
@@ -37,6 +79,6 @@ if __name__ == '__main__':
         exit(2)
 
     # 如果软件压缩包是完好的，则部署，并更新软件版本文件
-    deploy(pkg_path, deploy_dir)
+    deploy(pkg_path, deploy_dir, version)
     with open(ver_fname, 'w') as fobj:
         fobj.write(r.text)
