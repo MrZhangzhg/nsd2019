73d5e7fe6f5239dcf738294778efc34a2e7bdeb6
MrZhangzhg <zhangzg@tedu.cn>
2019-05-25 14:55:50 +0800

    1812 devweb day03 14:50

--- nsd1812/devweb/day03/mysite/polls/urls.py
+++ nsd1812/devweb/day03/mysite/polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # url(正则, 函数, name=该url的名字),
     url(r'^$', views.index, name='index'),
+    url(r'^news/$', views.news, name='news'),
 ]
--- nsd1812/devweb/day03/mysite/polls/views.py
+++ nsd1812/devweb/day03/mysite/polls/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def news(request):
+    return render(request, 'news.html')
