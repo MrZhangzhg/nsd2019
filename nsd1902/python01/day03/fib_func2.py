c97ff8d76a6c90444bf8c4734669aa0ccc1ff604
MrZhangzhg <zhangzg@tedu.cn>
2019-07-03 15:20:45 +0800

    nsd1902 py01 day03 15:20

---
+++ nsd1902/python01/day03/fib_func2.py
@@ -0,0 +1,25 @@
+# def fib_func():
+#     fib = [0, 1]
+#
+#     n = int(input('长度: '))
+#     for i in range(n - 2):
+#         fib.append(fib[-1] + fib[-2])
+#
+#     print(fib)
+#
+# a = fib_func()   # 函数默认返回None
+# print(a)
+
+
+def fib_func():
+    fib = [0, 1]
+
+    n = int(input('长度: '))
+    for i in range(n - 2):
+        fib.append(fib[-1] + fib[-2])
+
+    return fib   # 函数的运行结果，用关键字return返回
+
+a = fib_func()
+print(a)
+
