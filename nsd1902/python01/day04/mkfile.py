60529d6c239be30bd5d8b1d21357bda71b2683c1
MrZhangzhg <zhangzg@tedu.cn>
2019-07-04 14:15:39 +0800

    1902 py01 day04 14:20

---
+++ nsd1902/python01/day04/mkfile.py
@@ -0,0 +1,13 @@
+def get_fname():
+    pass
+
+def get_content():
+    pass
+
+def wfile(fname, content):
+    pass
+
+if __name__ == '__main__':
+    fname = get_fname()
+    content = get_content()
+    wfile(fname, content)
