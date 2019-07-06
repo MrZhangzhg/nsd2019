66946b1082cbf020f77ef582205319c8839b2f27
MrZhangzhg <zhangzg@tedu.cn>
2019-07-06 11:31:27 +0800

    1902 py01 day05 11:30

--- nsd1902/python01/day05/stack.py
+++ nsd1902/python01/day05/stack.py
@@ -1,14 +1,35 @@
 def push_it():
-    pass
+    print('push')
 
 def pop_it():
-    pass
+    print('pop')
 
 def view_it():
-    pass
+    print('view')
 
 def show_menu():
-    pass
+    prompt = """(0) 压栈
+(1) 出栈
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
+
+    while True:
+        # 删除用户输入字符串两端的空格
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2', '3']:
+            print('无效输入，请重试')
+            continue
+
+        if choice == '0':
+            push_it()
+        elif choice == '1':
+            pop_it()
+        elif choice == '2':
+            view_it()
+        else:
+            print('bye-bye')
+            break
 
 if __name__ == '__main__':
     show_menu()
