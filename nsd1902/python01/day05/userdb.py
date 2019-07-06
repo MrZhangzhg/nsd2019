5ef714ef8b49ff26765f5d98dcf41691a194cf06
MrZhangzhg <zhangzg@tedu.cn>
2019-07-06 15:36:29 +0800

    1902 py01 day05 15:40

--- nsd1902/python01/day05/userdb.py
+++ nsd1902/python01/day05/userdb.py
@@ -1,8 +1,24 @@
+import getpass
+
+userdb = {}
+
 def register():
-    print('register')
+    username = input('用户名: ')
+    # 如果用户已存在则给出提示，否则读入密码并写入字典
+    if username in userdb:
+        print('\033[31;1m用户已存在\033[0m')
+    else:
+        password = input('密码: ')
+        userdb[username] = password
 
 def login():
-    print('login')
+    username = input('用户名: ')
+    password = getpass.getpass('密码: ')
+    # if username in userdb and userdb[username] == password:
+    if userdb.get(username) == password:
+        print('\033[32;1m登陆成功\033[0m')
+    else:
+        print('\033[31;1m登陆失败\033[0m')
 
 def show_menu():
     cmds = {'0': register, '1': login}
@@ -10,7 +26,7 @@ def show_menu():
 (1) 登陆
 (2) 退出
 请选择(0/1/2): """
-    
+
     while True:
         # 将用户输入的两端空白去掉后，赋值给choice
         choice = input(prompt).strip()
@@ -19,7 +35,7 @@ def show_menu():
             continue
 
         if choice == '2':
-            print('Bye-buy')
+            print('Bye-bye')
             break
 
         cmds[choice]()
