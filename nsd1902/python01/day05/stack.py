11311a214efca9b22206f4827f594bd537add9c9
MrZhangzhg <zhangzg@tedu.cn>
2019-07-06 11:45:51 +0800

    1902 py01 day05 11:40

--- nsd1902/python01/day05/stack.py
+++ nsd1902/python01/day05/stack.py
@@ -1,13 +1,15 @@
-def push_it():
-    print('push')
-
 def pop_it():
     print('pop')
+
+def push_it():
+    print('push')
 
 def view_it():
     print('view')
 
 def show_menu():
+    # 把函数保存到字典中
+    cmds = {'0': pop_it, '1': push_it, '2': view_it}
     prompt = """(0) 压栈
 (1) 出栈
 (2) 查询
@@ -21,15 +23,21 @@ def show_menu():
             print('无效输入，请重试')
             continue
 
-        if choice == '0':
-            push_it()
-        elif choice == '1':
-            pop_it()
-        elif choice == '2':
-            view_it()
-        else:
-            print('bye-bye')
+        if choice == '3':
+            print('Bye-bye')
             break
+
+        cmds[choice]()  # 从字典中取出函数并调用
+
+        # if choice == '0':
+        #     push_it()
+        # elif choice == '1':
+        #     pop_it()
+        # elif choice == '2':
+        #     view_it()
+        # else:
+        #     print('bye-bye')
+        #     break
 
 if __name__ == '__main__':
     show_menu()
