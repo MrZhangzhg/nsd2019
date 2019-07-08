a170caccff7fd4b7b3f0b5fa9f5ebbbb06840e35
MrZhangzhg <zhangzg@tedu.cn>
2019-07-08 14:38:36 +0800

    1902 py02 day01 14:40

--- nsd1902/python02/day01/myerr.py
+++ nsd1902/python02/day01/myerr.py
@@ -1,13 +1,40 @@
+# try:
+#     num = int(input('number: '))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except ValueError:
+#     print('无效的输入')
+# except ZeroDivisionError:
+#     print('无效的输入')
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+# except EOFError:
+#     print('\nBye-bye')
+
+
+# try:
+#     num = int(input('number: '))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError):
+#     print('无效的输入')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+
 try:
     num = int(input('number: '))
     result = 100 / num
+except (ValueError, ZeroDivisionError):
+    print('无效的输入')
+except (KeyboardInterrupt, EOFError):
+    print('\nBye-bye')
+    exit(65)
+else:
     print(result)
+finally:
     print('Done')
-except ValueError:
-    print('无效的输入')
-except ZeroDivisionError:
-    print('无效的输入')
-except KeyboardInterrupt:
-    print('\nBye-bye')
-except EOFError:
-    print('\nBye-bye')
+
+print('正常结束')
