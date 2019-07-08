9a1f01561862ca69b366b4154d145b05bd4a4cff
MrZhangzhg <zhangzg@tedu.cn>
2019-07-08 15:45:37 +0800

    1902 py02 day01 15:50

--- nsd1902/python02/day01/myerr2.py
+++ nsd1902/python02/day01/myerr2.py
@@ -1,8 +1,14 @@
 def set_age(name, age):
     if not 0 < age < 120:
+        # 如果age没有在此范围抛出ValueError异常
         raise ValueError('年龄超出范围(1-120)')
 
     print('%s is %s years old.' % (name, age))
 
+def set_age2(name, age):
+    assert 0 < age < 120, '年龄超出范围(1-120)'
+    print('%s is %s years old.' % (name, age))
+
 if __name__ == '__main__':
-    set_age('杨艳龙', 266)
+    set_age('杨艳龙', 26)
+    set_age2('李家豪', 222)
