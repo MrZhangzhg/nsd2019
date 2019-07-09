c34737281791d6840e6ee0da121b04904fd2c7fa
MrZhangzhg <zhangzg@tedu.cn>
2019-07-09 16:39:35 +0800

    1902 py02 day02 16:40

--- nsd1902/python02/day02/day02.py
+++ nsd1902/python02/day02/day02.py
@@ -1,8 +1,23 @@
-def foo():
-    print('in foo')
-    bar()
+# def foo():
+#     print('in foo')
+#     bar()
+#
+# def bar():
+#     print('in bar')
+#
+# foo()
 
-def bar():
-    print('in bar')
+def func1(x):
+    if x == 1:
+        return x
+    return x * func1(x - 1)
+         # 5 * func1(4)
+         # 5 * 4 * func(3)
+         # 5 * 4 * 3 * func(2)
+         # 5 * 4 * 3 * 2 * func(1)
+         # 5 * 4 * 3 * 2 * 1
 
-foo()
+print(func1(5))
+
+
+
