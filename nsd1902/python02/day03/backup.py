9384883291160db0c5a892605f4e29dc911235e2
MrZhangzhg <zhangzg@tedu.cn>
2019-07-10 11:22:59 +0800

    1902 py02 day03 11:20

--- nsd1902/python02/day03/backup.py
+++ nsd1902/python02/day03/backup.py
@@ -1,7 +1,22 @@
+import hashlib
 import tarfile
 from time import strftime
 import os
 
+import pickle
+
+
+def check_md5(fname):
+    m = hashlib.md5()
+
+    with open(fname, 'rb') as fobj:
+        while True:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+
+    return m.hexdigest()
 
 def full_backup(src, dst, md5file):
     fname = '%s_full_%s.tar.gz' % (os.path.basename(src), strftime('%Y%m%d'))
@@ -13,16 +28,24 @@ def full_backup(src, dst, md5file):
     tar.close()
 
     # 计算每个文件的md5值
-    
+    md5dict = {}
+    for path, folders, files in os.walk(src):
+        for file in files:
+            key = os.path.join(path, file)
+            md5dict[key] = check_md5(key)
+
+    # 把md5值字典保存到文件
+    with open(md5file, 'wb') as fobj:
+        pickle.dump(md5dict, fobj)
 
 def incr_backup(src, dst, md5file):
-
+    pass
 
 if __name__ == '__main__':
     src = '/tmp/demo/security'
     dst = '/tmp/demo/backup'
     md5file = '/tmp/demo/backup/md5.data'
-    if strftime('%a') == 'Mon':
+    if strftime('%a') != 'Mon':
         full_backup(src, dst, md5file)
     else:
         incr_backup(src, dst, md5file)
