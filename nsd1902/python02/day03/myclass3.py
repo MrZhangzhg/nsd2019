c54618f71032e1c2377b80c5ec5c4c2341013683
MrZhangzhg <zhangzg@tedu.cn>
2019-07-10 15:43:36 +0800

    1902 py02 day03 15:40

--- nsd1902/python02/day03/myclass3.py
+++ nsd1902/python02/day03/myclass3.py
@@ -10,10 +10,16 @@ class Role:
         print('running...')
 
 class Warrior(Role):
+    def __init__(self, name, gender, weapon):
+        # 以下两种写法都可以调用父类的方法，未加注释的是推荐写法
+        # Role.__init__(self, name, weapon)
+        super(Warrior, self).__init__(name, weapon)
+        self.gender = gender
+
     def show_me(self):
         print("我是%s, 我是一个战士" % self.name)
 
 if __name__ == '__main__':
-    gy = Warrior('关羽', '青龙偃月刀')
+    gy = Warrior('关羽', '男', '青龙偃月刀')
     gy.speak('过五关，斩六将')
     gy.show_me()
