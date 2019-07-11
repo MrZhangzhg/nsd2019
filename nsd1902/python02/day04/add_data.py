22406c0539d4a1b052a916198878d56df3b7e1c5
MrZhangzhg <zhangzg@tedu.cn>
2019-07-11 15:35:10 +0800

    1902 py02 day04 15:40

--- nsd1902/python02/day04/add_data.py
+++ nsd1902/python02/day04/add_data.py
@@ -13,21 +13,31 @@ conn = pymysql.connect(
 # 创建游标，用于将来执行SQL语句
 cursor = conn.cursor()
 #################################
-# 向部门表中插入数据
+# 1. 向部门表中插入数据
 # 编写SQL语句
-insert_dep = 'INSERT INTO departments VALUES (%s, %s)'
+# insert_dep = 'INSERT INTO departments VALUES (%s, %s)'
 # 填写每个部门的字段值
-hr = (1, '人事部')
-ops = (2, '运维部')
-dev = (3, '开发部')
-qa = (4, '测试部')
-market = (5, '市场部')
-deps = [ops, dev, qa]
+# hr = (1, '人事部')
+# ops = (2, '运维部')
+# dev = (3, '开发部')
+# qa = (4, '测试部')
+# market = (5, '市场部')
+# deps = [ops, dev, qa]
 # 执行sql语句
-cursor.execute(insert_dep, hr)   # 插入一条数据
-cursor.executemany(insert_dep, deps)  # 插入多条数据
-cursor.executemany(insert_dep, [market])
+# cursor.execute(insert_dep, hr)   # 插入一条数据
+# cursor.executemany(insert_dep, deps)  # 插入多条数据
+# cursor.executemany(insert_dep, [market])
 ##################################
+# 2. 删除市场部
+del_dep = 'DELETE FROM departments WHERE dep_name=%s'
+cursor.execute(del_dep, ('市场部',))
+# 3. 修改
+update_dep = 'UPDATE departments SET dep_name=%s WHERE dep_name=%s'
+cursor.execute(update_dep, ('人力资源部', '人事部'))
+
+
+##################################
+
 # 确认
 conn.commit()
 
