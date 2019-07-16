d3abe17ce274e0ab04b812c3bb7361288042b167
MrZhangzhg <zhangzg@tedu.cn>
2019-07-16 14:37:50 +0800

    1902 devops day03 14:40

--- nsd1902/devops/day03/adhoc.py
+++ nsd1902/devops/day03/adhoc.py
@@ -1,4 +1,3 @@
-import json
 import shutil
 from collections import namedtuple
 from ansible.parsing.dataloader import DataLoader
@@ -6,23 +5,7 @@ from ansible.vars.manager import VariableManager
 from ansible.inventory.manager import InventoryManager
 from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
-from ansible.plugins.callback import CallbackBase
 import ansible.constants as C
-
-class ResultCallback(CallbackBase):
-    """A sample callback plugin used for performing an action as results come in
-
-    If you want to collect all results into a single object for processing at
-    the end of the execution, look into utilizing the ``json`` callback plugin
-    or writing your own custom callback plugin
-    """
-    def v2_runner_on_ok(self, result, **kwargs):
-        """Print a json representation of the result
-
-        This method could store the result in an instance attribute for retrieval later
-        """
-        host = result._host
-        print(json.dumps({host.name: result._result}, indent=4))
 
 # since API is constructed for CLI it expects certain options to always be set, named tuple 'fakes' the args parsing options object
 Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
@@ -31,9 +14,6 @@ options = Options(connection='local', module_path=['/to/mymodules'], forks=10, b
 # initialize needed objects
 loader = DataLoader() # Takes care of finding and reading yaml, json and ini files
 passwords = dict(vault_pass='secret')
-
-# Instantiate our ResultCallback for handling results as they come in. Ansible expects this to be one of its main display outlets
-results_callback = ResultCallback()
 
 # create inventory, use path to host config file as source or hosts in a comma separated string
 inventory = InventoryManager(loader=loader, sources='localhost,')
@@ -65,7 +45,6 @@ try:
               loader=loader,
               options=options,
               passwords=passwords,
-              stdout_callback=results_callback,  # Use our custom callback instead of the ``default`` callback plugin, which prints to stdout
           )
     result = tqm.run(play) # most interesting data for a play is actually sent to the callback's methods
 finally:
