8410d1cae66b11509a9eddf020d72ea0560efbb0
MrZhangzhg <zhangzg@tedu.cn>
2019-07-26 11:50:23 +0800

    py1902 webansi day06 11:50

--- nsd1902/devweb/ansible_pro/myansible/webadmin/views.py
+++ nsd1902/devweb/ansible_pro/myansible/webadmin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import HostGroup, Module
+from .models import HostGroup, Module, Host
 
 # Create your views here.
 
@@ -33,4 +33,8 @@ def add_modules(request):
     return render(request, 'addmodules.html', {'modules': modules})
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    hosts = Host.objects.all()
+    groups = HostGroup.objects.all()
+    modules = Module.objects.all()
+    data = {'hosts': hosts, 'groups': groups, 'modules': modules}
+    return render(request, 'tasks.html', data)
