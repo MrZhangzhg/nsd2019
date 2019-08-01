40bec9e59b529225593de21044ccf1b01ed3ba9a
MrZhangzhg <zhangzg@tedu.cn>
2019-08-01 14:38:58 +0800

    nsd1903 py01 day02 14:40

---
+++ nsd1903/python01/day02/game5.py
@@ -0,0 +1,30 @@
+import random
+
+all_choices = ['石头', '剪刀', '布']
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+prompt = '''(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): '''
+cwin = 0
+pwin = 0
+
+# 人机赢的次数都不够2次才继续
+while True:
+    computer = random.choice(all_choices)
+    ind = int(input(prompt))
+    player = all_choices[ind]
+
+    print("Your choice: %s, Computer's choice: %s" % (player, computer))
+    if player == computer:
+        print('\033[32;1m平局\033[0m')
+    elif [player, computer] in win_list:
+        print('\033[31;1mYou WIN!!!\033[0m')
+        pwin += 1
+    else:
+        print('\033[31;1mYou LOSE!!!\033[0m')
+        cwin += 1
+
+    # 人机只要有一方赢够2次就结束循环
+    if pwin == 2 or cwin == 2:
+        break
