aab3c1855811862a367933ec4a76659aa5db678b
MrZhangzhg <zhangzg@tedu.cn>
2019-08-01 14:54:31 +0800

    nsd1903 py01 day02 14:50

---
+++ nsd1903/python01/day02/while2.py
@@ -0,0 +1,34 @@
+# 100以内的偶数之和
+
+# result = 0
+# counter = 0
+#
+# while counter < 100:
+#     counter += 1
+#
+#     if counter % 2 == 1:
+#         continue
+#     else:
+#         result += counter
+
+# result = 0
+# counter = 0
+#
+# while counter < 100:
+#     counter += 1
+#
+#     if counter % 2 == 1:
+#         continue
+#
+#     result += counter
+
+result = 0
+counter = 0
+
+while counter < 100:
+    counter += 1
+
+    if counter % 2:   # 求余只有0和1两种情况，非0为真，0为假
+        continue
+
+    result += counter
