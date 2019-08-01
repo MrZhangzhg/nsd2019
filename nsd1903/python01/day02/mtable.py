eccb4bb42ab893bbdc76775fe1806142ad0a1cb7
MrZhangzhg <zhangzg@tedu.cn>
2019-08-01 17:19:01 +0800

    nsd1903 py01 day02 17:20

--- nsd1903/python01/day02/mtable.py
+++ nsd1903/python01/day02/mtable.py
@@ -14,8 +14,13 @@
 #         print('hello', end=' ')
 #     print()
 ###################################
+# for i in range(1, 10):
+#     for j in range(1, i + 1):
+#         print('%sx%s=%s' % (j, i, i * j), end=' ')
+#     print()
 
+###################################
 for i in range(1, 10):
     for j in range(1, i + 1):
-        print('%sx%s=%s' % (j, i, i * j), end=' ')
+        print('*', end=' ')
     print()
