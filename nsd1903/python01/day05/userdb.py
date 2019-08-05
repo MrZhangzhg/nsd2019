9756590d28ae3f36c5646910e10b281560b90f36
MrZhangzhg <zhangzg@tedu.cn>
2019-08-05 15:54:39 +0800

    nsd1903 py01 day05 16:00

--- nsd1903/python01/day05/userdb.py
+++ nsd1903/python01/day05/userdb.py
@@ -1,8 +1,27 @@
+import getpass
+
+userdb = {}   # 用于存储用户名和密码
+
 def register():
-    print('注册')
+    username = input('用户名: ').strip()
+    if username == '':
+        print('用户名不能为空')
+    elif not username.isalnum():
+        print('用户名只能包含字母和数字')
+    elif username in userdb:
+        print('用户已存在')
+    else:
+        password = input('密码: ')
+        userdb[username] = password
 
 def login():
-    print('登陆')
+    username = input('用户名: ').strip()
+    password = getpass.getpass('密码: ').strip()
+    # if username not in userdb or userdb[username] != password:
+    if userdb.get(username) != password:
+        print('\033[31;1m登陆失败\033[0m')
+    else:
+        print('\033[32;1m登陆成功\033[0m')
 
 def show_menu():
     cmds = {'0': register, '1': login}
