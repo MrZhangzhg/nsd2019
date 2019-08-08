fd98e721428fb714c38b8410d45191e520bf532e
MrZhangzhg <zhangzg@tedu.cn>
2019-08-08 11:58:16 +0800

    nsd1903 py02 day02 12:00

--- nsd1903/python02/day02/math_game.py
+++ nsd1903/python02/day02/math_game.py
@@ -12,18 +12,34 @@ def exam():
 
     # 让用户做答，判断对错
     prompt = '%s %s %s = ' % (nums[0], op, nums[1])
-    answer = int(input(prompt))
-    if answer == result:
-        print('非常棒!!!')
-    else:
+    counter = 0
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:    # 不指定异常可以捕获所有异常，但是不推荐
+            print()
+            continue
+
+        if answer == result:
+            print('非常棒!!!')
+            break
         print('不对哟!!!')
+        counter += 1
+    else:
+        print('%s%s' % (prompt, result))
 
 def main():
     "该函数先出题，然后询问用户是否继续"
     while True:
         exam()
         # 去除字符串两端空白字符后，取出第一个字符
-        yn = input('Continue(y/n)? ').strip()[0]
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]
+        except IndexError:
+            yn = 'y'
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
         if yn in 'nN':
             print('\nBye-bye')
             break
