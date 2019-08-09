e2ce95b2a5a01e3bac1ae9658bc57ef1fb17e350
MrZhangzhg <zhangzg@tedu.cn>
2019-08-09 14:26:20 +0800

    nsd1903 py02 day03 14:30

---
+++ nsd1903/python02/day03/myclass3.py
@@ -0,0 +1,27 @@
+class GameRole:
+    def __init__(self, nm, wp):
+        self.name = nm
+        self.weapon = wp
+
+    def show_me(self):
+        print('我是%s, 我的武器是%s' % (self.name, self.weapon))
+
+    def speak(self, content):
+        print('%s在此, %s' % (self.name, content))
+
+class Warrior(GameRole):  # GameRole称作父类或基类
+    "子类可以继承父类的属性"
+    def attack(self):
+        print('近身肉搏')
+
+class Mage(GameRole):
+    def attack(self):
+        print('远程攻击')
+
+if __name__ == '__main__':
+    nz = Mage('哪吒', '火尖枪')
+    hfh = Warrior('黄飞虎', '鞭')
+    nz.show_me()
+    hfh.show_me()
+    nz.attack()
+    hfh.attack()
