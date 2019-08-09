f4299e749faac94a2cb86f981ed57e2308970238
MrZhangzhg <zhangzg@tedu.cn>
2019-08-09 11:47:04 +0800

    nsd1903 py02 day03 11:50

--- nsd1903/python02/day03/myclass.py
+++ nsd1903/python02/day03/myclass.py
@@ -9,6 +9,9 @@ class GameRole:
     def speak(self, content):
         print('%s在此, %s' % (self.name, content))
 
+    def attack(self, dest):
+        print('正在攻击: %s' % dest)
+
 if __name__ == '__main__':
     # 创建实例时，自动调用__init__方法
     # 调用通过类创建的方法，实例自动作为第一个参数传递
@@ -16,3 +19,10 @@ if __name__ == '__main__':
     print(lb.name, lb.weapon)
     lb.show_me()
     lb.speak('人在塔在')
+
+    zf = GameRole('张飞', '丈八蛇矛')
+    zf.show_me()
+    zf.speak('燕人张飞')
+
+    zf.attack('吕布')
+    lb.attack('刘备')
