fe4ae77874325ca4510827e2e9e04ef334962ea2
MrZhangzhg <zhangzg@tedu.cn>
2019-08-09 10:29:03 +0800

    nsd1903 py02 day03 10:30

--- nsd1903/python02/day03/backup.py
+++ nsd1903/python02/day03/backup.py
@@ -39,7 +39,32 @@ def full_backup(src, dst, md5file):
         pickle.dump(md5dict, fobj)
 
 def incr_backup(src, dst, md5file):
-    pass
+    "增量备份把新增文件和改动的文件打包；更新md5文件以便于后续比较"
+    # 备份的tar文件要有备份目录名、备份类型、时间
+    fname = '%s_incr_%s.tar.gz' % (os.path.basename(src), strftime('%Y%m%d'))
+    fname = os.path.join(dst, fname)
+
+    # 计算每个文件的md5值，将其存入字典
+    md5dict = {}
+    for path, folders, files in os.walk(src):
+        for file in files:
+            key = os.path.join(path, file)
+            md5dict[key] = check_md5(key)
+
+    # 取出前一天的md5值
+    with open(md5file, 'rb') as fobj:
+        old_md5 = pickle.load(fobj)
+
+    # 将新增文件和有改动文件进行打包
+    tar = tarfile.open(fname, 'w:gz')
+    for key in md5dict:
+        if old_md5.get(key) != md5dict[key]:
+            tar.add(key)
+    tar.close()
+
+    # 更新md5字典文件
+    with open(md5file, 'wb') as fobj:
+        pickle.dump(md5dict, fobj)
 
 if __name__ == '__main__':
     src = '/tmp/demo/security'
