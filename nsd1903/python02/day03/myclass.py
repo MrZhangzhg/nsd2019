9224016f7180da2d617f3a7cb8ac8a509bda7876
MrZhangzhg <zhangzg@tedu.cn>
2019-08-09 11:35:44 +0800

    nsd1903 py02 day03 11:40

--- nsd1903/python02/day03/myclass.py
+++ nsd1903/python02/day03/myclass.py
@@ -3,8 +3,16 @@ class GameRole:
         self.name = nm
         self.weapon = wp
 
+    def show_me(self):
+        print('我是%s, 我的武器是%s' % (self.name, self.weapon))
+
+    def speak(self, content):
+        print('%s在此, %s' % (self.name, content))
+
 if __name__ == '__main__':
     # 创建实例时，自动调用__init__方法
     # 调用通过类创建的方法，实例自动作为第一个参数传递
     lb = GameRole('吕布', '方天画戟')  # 创建名为lb的实例
     print(lb.name, lb.weapon)
+    lb.show_me()
+    lb.speak('人在塔在')
