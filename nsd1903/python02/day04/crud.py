b8d3ef6e8946dc666568d9b3e73affd5e6135f91
MrZhangzhg <zhangzg@tedu.cn>
2019-08-10 14:16:24 +0800

    nsd1903 py02 day04 14:20

--- nsd1903/python02/day04/crud.py
+++ nsd1903/python02/day04/crud.py
@@ -10,9 +10,24 @@ conn = pymysql.connect(
 )
 cursor = conn.cursor()
 #################################
-insert = "INSERT INTO departments VALUES(%s, %s)"
-deps = [(1, '人事部'), (2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '市场部'), (6, '销售部')]
-cursor.executemany(insert, deps)
+# 添加数据
+# insert = "INSERT INTO departments VALUES(%s, %s)"
+# deps = [(1, '人事部'), (2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '市场部'), (6, '销售部')]
+# cursor.executemany(insert, deps)
+#################################
+# 查询并取出查询结果
+select = "SELECT * FROM departments"
+cursor.execute(select)
+result1 = cursor.fetchone()  # 取出一个记录
+print(result1)
+print('*' * 30)
+result2 = cursor.fetchmany(2)  # 取出2个记录
+print(result2)
+print('*' * 30)
+result3 = cursor.fetchall()  # 取出全部记录
+print(result3)
+
+
 
 #################################
 conn.commit()
