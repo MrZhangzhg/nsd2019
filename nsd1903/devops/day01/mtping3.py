161e5dfa725f765a4bedf61ecbc0167bae8ad487
MrZhangzhg <zhangzg@tedu.cn>
2019-08-12 15:15:31 +0800

    nsd1903 devops day01 15:10

---
+++ nsd1903/devops/day01/mtping3.py
@@ -0,0 +1,22 @@
+import subprocess
+import threading
+
+class Ping:
+    def __init__(self, host):
+        self.host = host
+
+    def __call__(self):
+        result = subprocess.run(
+            'ping -c2 %s &> /dev/null' % self.host,
+            shell=True
+        )
+        if result.returncode == 0:
+            print('%s:up' % self.host)
+        else:
+            print('%s:down' % self.host)
+
+if __name__ == '__main__':
+    ips = ['172.40.59.%s' % i for i in range(1, 255)]
+    for ip in ips:
+        t = threading.Thread(target=Ping(ip))
+        t.start()  # target(*args)
