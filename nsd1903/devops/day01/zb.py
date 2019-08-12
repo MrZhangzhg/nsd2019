ebb91f08f8b3d19b22897542de9dfe264533c606
MrZhangzhg <zhangzg@tedu.cn>
2019-08-12 11:03:46 +0800

    nsd1903 devops day01 11:10

---
+++ nsd1903/devops/day01/zb.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+print('starting...')
+retval = os.fork()
+if retval:
+    print('父进程')
+    time.sleep(30)
+    print('父进程，醒了')
+else:
+    print('子进程')
+    time.sleep(10)
+    print('子进程，醒了')
+    exit()
+
+
