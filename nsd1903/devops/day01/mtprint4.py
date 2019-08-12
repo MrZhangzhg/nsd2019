3a1663a517a8b871965ef9af3b606de8971c4fa7
MrZhangzhg <zhangzg@tedu.cn>
2019-08-12 14:42:44 +0800

    nsd1903 devops day01 14:40

---
+++ nsd1903/devops/day01/mtprint4.py
@@ -0,0 +1,11 @@
+import threading
+
+class MyClass:
+    def __call__(self, a, b, c):
+        print('Hello', a, b, c)
+
+if __name__ == '__main__':
+    a = MyClass()
+    for i in range(3):
+        t = threading.Thread(target=MyClass(), args=(10, 20, 30))
+        t.start()  # target(*args)
