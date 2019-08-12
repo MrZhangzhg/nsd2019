81bccf77c8e79fe5d3b2db9f327d3356b60354c9
MrZhangzhg <zhangzg@tedu.cn>
2019-08-12 16:37:16 +0800

    nsd1903 devops day01 16:40

--- nsd1903/devops/day01/get_web.py
+++ nsd1903/devops/day01/get_web.py
@@ -1,6 +1,7 @@
 import wget
 import os
-from urllib import request
+import re
+from urllib import request, error
 
 def download(url, fname):
     html = request.urlopen(url)
@@ -12,13 +13,32 @@ def download(url, fname):
             fobj.write(data)
 
 if __name__ == '__main__':
+    # 创建存储图片的目录
     download_dir = '/tmp/163'
     if not os.path.exists(download_dir):
         os.mkdir(download_dir)
 
+    # 下载网易首页
     url163 = 'http://www.163.com'
     fname163 = '/tmp/163/163.html'
-    download(url163, fname163)
+    if not os.path.exists(fname163):
+        download(url163, fname163)
 
+    # 在网页中取出图片url
+    img_list = []
+    img_patt = '(http|https)://[-./\w]+\.(jpg|jpeg|png|gif)'
+    cpatt = re.compile(img_patt)
+    # open默认采用utf8编码，如果不是utf8，需要指明
+    with open(fname163, encoding='gbk') as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:
+                img_list.append(m.group())
+    # print(img_list)
 
-
+    # 下载图片
+    for img_url in img_list:
+        try:
+            wget.download(img_url, download_dir)
+        except error.HTTPError:
+            pass
