675923777a71720ddb6f929cec2b08818e10175d
MrZhangzhg <zhangzg@tedu.cn>
2019-08-17 15:48:37 +0800

    nsd1903 devops day05 16:00

--- nsd1903/devops/day05/deploy.py
+++ nsd1903/devops/day05/deploy.py
@@ -1,6 +1,24 @@
+import os
+import requests
 
-def has_new_ver():
+def has_new_ver(ver_url, ver_fname):
     "有新版本返回True，否则返回False"
+    # 本地没有版本文件，返回True
+    if not os.path.exists(ver_fname):
+        return True
+
+    # 取出本地版本文件内容
+    with open(ver_fname) as fobj:
+        version = fobj.read()
+
+    # 取出远程最新版本号
+    r = requests.get(ver_url)
+
+    # 判断版本号一致为False，不一致为True
+    if version == r.text:
+        return False
+    else:
+        return True
 
 def check_app():
     "校验软件包，未损坏返回True，损坏返回False"
@@ -10,7 +28,10 @@ def deploy():
 
 if __name__ == '__main__':
     # 如果未发现新版本，则退出
-    if not has_new_ver():
+    dep_dir = '/var/www/deploy'
+    ver_url = 'http://192.168.4.7/deploy/live_ver'
+    ver_fname = os.path.join(dep_dir, 'live_ver')
+    if not has_new_ver(ver_url, ver_fname):
         print('未发现新版本。')
         exit(1)
 
