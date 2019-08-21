2d714a40af31e06639035ead058e583e393567cb
MrZhangzhg <zhangzg@tedu.cn>
2019-08-21 17:48:46 +0800

    nsd1903 devweb day03 17:50

--- nsd1903/devweb/day03/mysite/polls/models.py
+++ nsd1903/devweb/day03/mysite/polls/models.py
@@ -13,3 +13,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question)
+
+    def __str__(self):
+        return "%s: %s" % (self.question, self.choice_text)
