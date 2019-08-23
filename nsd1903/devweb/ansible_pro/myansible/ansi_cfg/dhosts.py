34b072a6ed657bbcc2495f4c9c0f126530b49d34
MrZhangzhg <zhangzg@tedu.cn>
2019-08-23 15:50:56 +0800

    nsd1903 devweb day05 15:50

--- nsd1903/devweb/ansible_pro/myansible/ansi_cfg/dhosts.py
+++ nsd1903/devweb/ansible_pro/myansible/ansi_cfg/dhosts.py
@@ -3,6 +3,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import json
 
 engine = create_engine(
     'sqlite:////var/ftp/nsd2019/nsd1903/devweb/ansible_pro/myansible/db.sqlite3',
@@ -17,10 +18,21 @@ class Group(Base):
     groupname = Column(String(50), unique=True)
 
 class Host(Base):
+    __tablename__ = 'webadmin_host'
     id = Column(Integer, primary_key=True)
     hostname = Column(String(50))
     ipaddr = Column(String(15))
     group_id = Column(Integer, ForeignKey('webadmin_group.id'))
 
 if __name__ == '__main__':
-    
+    session = Session()
+    qset = session.query(Group.groupname, Host.ipaddr).join(Host)
+    # print(qset.all())
+    # [('dbservers', '192.168.4.5'), ('webservers', '192.168.4.7'), ('webservers', '192.168.4.6')]
+    result = {}
+    for g, ip in qset:
+        if g not in result:
+            result[g] = {}  # {'dbservers': {}}
+            result[g]['hosts'] = []  # {'dbservers': {'hosts': []}}
+        result[g]['hosts'].append(ip)
+    print(json.dumps(result))
