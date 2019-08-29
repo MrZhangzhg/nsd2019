a91e28d5ee16954a756cf52928d20fe771607d8c
MrZhangzhg <zhangzg@tedu.cn>
2019-08-29 16:28:24 +0800

    nsd1904 py01 day02 16:26

--- nsd1904/py01/day02/guess.py
+++ nsd1904/py01/day02/guess.py
@@ -1,4 +1,17 @@
 import random
 
-print(random.randint(1, 100))
+counter = 0
+num = random.randint(1, 100)
 
+while counter < 7:   # 最多5次机会
+    counter += 1
+    answer = int(input('guess: '))
+    if answer < num:
+        print('猜小了')
+    elif answer > num:
+        print('猜大了')
+    else:
+        print('猜对了')
+        break
+else:   # 循环被break就不执行else，否则执行
+    print('answer is: %s' % num)
