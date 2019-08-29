cbb458943c902f1a45caf30bb4d929c0b6d1b749
MrZhangzhg <zhangzg@tedu.cn>
2019-08-29 16:58:10 +0800

    nsd1904 py01 day02 17:06

---
+++ nsd1904/py01/day02/for1.py
@@ -0,0 +1,31 @@
+# astr = 'abc'
+# alist = [10, 20, 30]
+# atup = ('tom', 'jerry')
+# adict = {'name': 'bob', 'age': 20}
+#
+# for ch in astr:
+#     print(ch)
+#
+# for i in alist:
+#     print(i)
+#
+# for name in atup:
+#     print(name)
+#
+# for key in adict:
+#     print(key, adict[key])
+
+
+# for i in range(10):
+#     print(i)
+
+result = 0
+for i in range(1, 10000001):
+    result += i
+
+print(result)
+
+
+
+
+
