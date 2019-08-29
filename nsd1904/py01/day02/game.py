a11260c9eacd07e052ded8e4b204fdc88809cc9a
MrZhangzhg <zhangzg@tedu.cn>
2019-08-29 11:50:49 +0800

    nsd1904 py01 day02 11:55

---
+++ nsd1904/py01/day02/game.py
@@ -0,0 +1,28 @@
+import random   # 导入random模块
+
+# random.choice()从一个序列对象中随机选择一项
+computer = random.choice(['石头', '剪刀', '布'])
+player = input('请出拳(石头/剪刀/布): ')
+
+print("Your choice: %s, Computer's choice: %s" % (player, computer))
+if player == '石头':
+    if computer == '石头':
+        print('平局')
+    elif computer == '剪刀':
+        print('You WIN!!!')
+    else:
+        print('You LOSE!!!')
+elif player == '剪刀':
+    if computer == '石头':
+        print('You LOSE!!!')
+    elif computer == '剪刀':
+        print('平局')
+    else:
+        print('You WIN!!!')
+else:
+    if computer == '石头':
+        print('You WIN!!!')
+    elif computer == '剪刀':
+        print('You LOSE!!!')
+    else:
+        print('平局')
