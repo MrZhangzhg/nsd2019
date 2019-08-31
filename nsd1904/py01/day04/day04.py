321cda7ec440a9e8091b8327d4f80a299ff999bc
MrZhangzhg <zhangzg@tedu.cn>
2019-08-31 15:22:31 +0800

    nsd1904 py01 day04 15:20

--- nsd1904/py01/day04/day04.py
+++ nsd1904/py01/day04/day04.py
@@ -1,1 +1,16 @@
-import shutil
+alist = ['bob', 'alice', 'tom', 'jerrey']
+
+for i in range(len(alist)):    # 常用
+    print(i, alist[i])
+
+print('*' * 30)
+
+print(list(enumerate(alist)))
+
+for item in enumerate(alist):
+    print(item)
+
+print('*' * 30)
+
+for i, name in enumerate(alist):   # 常用
+    print(i, name)
