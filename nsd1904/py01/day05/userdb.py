8332699061f4639842ec42bd86c8e0d4d1dfda99
MrZhangzhg <zhangzg@tedu.cn>
2019-09-02 16:03:32 +0800

    nsd1904 py01 day05 16:00

---
+++ nsd1904/py01/day05/userdb.py
@@ -0,0 +1,48 @@
+import getpass
+
+userdb = {}
+
+def register():
+    "注册"
+    username = input('用户名: ').strip()
+    if not username:
+        print('用户名不能为空')
+    elif username in userdb:
+        print('用户已存在')
+    else:
+        password = input('密码: ')
+        userdb[username] = password
+
+
+def login():
+    "登陆"
+    username = input('用户名: ')
+    password = getpass.getpass('密码: ')
+    # if username in userdb and userdb[username] == password:
+    if userdb.get(username) == password:
+        print('登陆成功')
+    else:
+        print('登陆失败')
+
+
+def show_menu():
+    cmds = {'0': register, '1': login}
+    "程序主菜单"
+    prompt = """(0) 注册
+(1) 登陆
+(2) 退出
+请选择(0/1/2): """
+    while True:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2']:
+            print('无效输入，请重试！')
+            continue
+
+        if choice == '2':
+            print('Bye-bye')
+            break
+
+        cmds[choice]()
+
+if __name__ == '__main__':
+    show_menu()
