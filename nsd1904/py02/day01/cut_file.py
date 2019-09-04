60c01b2e606f7784ce078a961a5a380874eb6edf
MrZhangzhg <zhangzg@tedu.cn>
2019-09-04 10:30:58 +0800

    nsd1904 py02 day02 10:30

---
+++ nsd1904/py02/day01/cut_file.py
@@ -0,0 +1,14 @@
+# 取出2019-05-15 9点到12点之间的数据
+import time
+
+fname = 'myfile.txt'
+t9 = time.strptime('2019-5-15 9:00:00', '%Y-%m-%d %H:%M:%S')
+t12 = time.strptime('2019-5-15 12:00:00', '%Y-%m-%d %H:%M:%S')
+
+with open(fname) as fobj:
+    for line in fobj:
+        # 文件每行的前19个字符是时间字符串
+        t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+        if t9 < t < t12:
+            print(line, end='')
+
