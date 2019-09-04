65d66780f544cca6a1ac3c1a6459eb4985f72b74
MrZhangzhg <zhangzg@tedu.cn>
2019-09-04 10:45:22 +0800

    nsd1904 py02 day02 10:40

--- nsd1904/py02/day01/cut_file.py
+++ nsd1904/py02/day01/cut_file.py
@@ -5,10 +5,19 @@ fname = 'myfile.txt'
 t9 = time.strptime('2019-5-15 9:00:00', '%Y-%m-%d %H:%M:%S')
 t12 = time.strptime('2019-5-15 12:00:00', '%Y-%m-%d %H:%M:%S')
 
+# with open(fname) as fobj:
+#     for line in fobj:
+#         # 文件每行的前19个字符是时间字符串
+#         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+#         if t9 < t < t12:
+#             print(line, end='')
+###########################
 with open(fname) as fobj:
     for line in fobj:
-        # 文件每行的前19个字符是时间字符串
         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
-        if t9 < t < t12:
+        if t > t12:
+            break
+        if t > t9:
             print(line, end='')
+
 
