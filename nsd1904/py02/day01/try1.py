024774b94170116a1c9672251183d1835673bf35
MrZhangzhg <zhangzg@tedu.cn>
2019-09-04 14:31:43 +0800

    nsd1904 py02 day02 14:30

--- nsd1904/py02/day01/try1.py
+++ nsd1904/py02/day01/try1.py
@@ -1,13 +1,65 @@
+# try:
+#     num = int(input("数字: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except ValueError:
+#     print('无效的输入')
+# except ZeroDivisionError:
+#     print('无效的输入')
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+# except EOFError:
+#     print('\nBye-bye')
+
+#########################################
+# try:
+#     num = int(input("数字: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError):
+#     print('无效的输入')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+#########################################
+# try:
+#     num = int(input("数字: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError) as e:  # 把异常保存到变量e
+#     print('无效的输入: %s' % e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+#########################################
+# try:
+#     num = int(input("数字: "))
+#     result = 100 / num
+# except (ValueError, ZeroDivisionError) as e:  # 把异常保存到变量e
+#     print('无效的输入: %s' % e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+# else:
+#     print(result)
+#
+# print('Done')
+
+#########################################
 try:
     num = int(input("数字: "))
     result = 100 / num
+except (ValueError, ZeroDivisionError) as e:  # 把异常保存到变量e
+    print('无效的输入: %s' % e)
+except (KeyboardInterrupt, EOFError):
+    print('\nBye-bye')
+    exit()   # 程序遇到exit，将彻底结束，不会再向下执行
+else:
     print(result)
-    print('Done')
-except ValueError:
-    print('无效的输入')
-except ZeroDivisionError:
-    print('无效的输入')
-except KeyboardInterrupt:
-    print('\nBye-bye')
-except EOFError:
-    print('\nBye-bye')
+finally:
+    print('异常不管是不是发生了，都要执行')
+
+print('Done')
+
