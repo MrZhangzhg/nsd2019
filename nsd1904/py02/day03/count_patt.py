d21bde5340b6e789635f8cc40cf32aaa85929783
MrZhangzhg <zhangzg@tedu.cn>
2019-09-06 16:55:56 +0800

    nsd1904 py02 day04 16:55

---
+++ nsd1904/py02/day03/count_patt.py
@@ -0,0 +1,10 @@
+def count_patt(fname, patt):
+
+
+
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = ''
+    br = ''
+    print(count_patt(fname, ip))
+    print(count_patt(fname, br))
