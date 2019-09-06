cd4b5ea1af55834cc2ce5c34d250fbd967a4c388
MrZhangzhg <zhangzg@tedu.cn>
2019-09-06 09:42:01 +0800

    nsd1904 py02 day04 9:40

---
+++ nsd1904/py02/day03/backup.py
@@ -0,0 +1,54 @@
+import os
+import tarfile
+import hashlib
+import pickle
+from time import strftime
+
+def check_md5(fname):
+    m = hashlib.md5()
+
+    with open(fname, 'rb') as fobj:
+        while True:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+
+    return m.hexdigest()
+
+
+def full_backup(src, dst, md5file):
+    "打包src目录，并计算src目录下每个文件的md5值"
+    # 拼接备份目标文件的绝对路径
+    fname = "%s_full_%s.tar.gz" % (os.path.basename(src), strftime('%Y%m%d'))
+    fname = os.path.join(dst, fname)
+
+    # 实现完全备份，把src目录打包
+    tar = tarfile.open(fname, 'w:gz')
+    tar.add(src)
+    tar.close()
+
+    # 计算每个文件的md5值
+    md5dict = {}
+    for path, folders, files in os.walk(src):
+        for file in files:
+            key = os.path.join(path, file)
+            md5dict[key] = check_md5(key)
+
+    # 将md5字典存入文件
+    with open(md5file, 'wb') as fobj:
+        pickle.dump(md5dict, fobj)
+
+
+def incr_backup(src, dst, md5file):
+
+
+
+if __name__ == '__main__':
+    src = '/tmp/demo/security'
+    dst = '/tmp/demo/backup'
+    md5file = '/tmp/demo/backup/md5.data'
+    if strftime('%a') == 'Mon':
+        full_backup(src, dst, md5file)
+    else:
+        incr_backup(src, dst, md5file)
