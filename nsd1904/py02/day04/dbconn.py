2fea667b67275582ea69f57d918179a0730ce5ca
MrZhangzhg <zhangzg@tedu.cn>
2019-09-07 15:31:17 +0800

    nsd1904 py02 day05 15:30

--- nsd1904/py02/day04/dbconn.py
+++ nsd1904/py02/day04/dbconn.py
@@ -1,12 +1,12 @@
 # MariaDB [nsd1904]> CREATE DATABASE tedu1904 DEFAULT CHARSET utf8;
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 # 创建连接mysql数据库的引擎
 engin = create_engine(
     # mysql+pymysql://用户:密码@服务器/数据库?参数
     'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1904?charset=utf8',
     encoding='utf8',
-    echo=True   # 打开debug日志，生产环境不要设置
+    # echo=True   # 打开debug日志，生产环境不要设置
 )
 Base = declarative_base()  # 生成实体类的基类
 
@@ -15,6 +15,15 @@ class Departments(Base):
     __tablename__ = 'departments'   # 指定类关联的表
     dep_id = Column(Integer, primary_key=True)
     dep_name = Column(String(20))
+
+class Employees(Base):
+    __tablename__ = 'employees'
+    emp_id = Column(Integer, primary_key=True)
+    emp_name = Column(String(20))
+    birth_date = Column(Date)
+    email = Column(String(50))
+    dep_id = Column(Integer, ForeignKey('departments.dep_id'))
+
 
 if __name__ == '__main__':
     # 如果库中没有相应的表则创建，有就不会再创建了
