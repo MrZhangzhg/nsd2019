bb373b2315789afb3dcc4f29ffcb50e00673ae12
MrZhangzhg <zhangzg@tedu.cn>
2019-09-07 14:10:20 +0800

    nsd1904 py02 day05 14:12

--- nsd1904/py02/day04/pymysql_crud.py
+++ nsd1904/py02/day04/pymysql_crud.py
@@ -13,11 +13,23 @@ conn = pymysql.connect(
 cur = conn.cursor()  # 创建游标，相当于文件对象
 ###################################
 # 添加部门
-insert_dep = 'INSERT INTO departments(dep_id, dep_name) VALUES(%s, %s)'
-hr = [(1, '人事部')]
-deps = [(2, '财务部'), (3, '运维部'), (4, '开发部'), (5, '测试部'), (6, '市场部')]
-cur.executemany(insert_dep, hr)
-cur.executemany(insert_dep, deps)
+# insert_dep = 'INSERT INTO departments(dep_id, dep_name) VALUES(%s, %s)'
+# hr = [(1, '人事部')]
+# deps = [(2, '财务部'), (3, '运维部'), (4, '开发部'), (5, '测试部'), (6, '市场部')]
+# cur.executemany(insert_dep, hr)
+# cur.executemany(insert_dep, deps)
+###################################
+# 查询
+select1 = 'SELECT * FROM departments ORDER BY dep_id'
+cur.execute(select1)
+result1 = cur.fetchone()  # 取出一项
+result2 = cur.fetchmany(2)  # 取出2项
+result3 = cur.fetchall()   # 取出全部
+print(result1)
+print('*' * 30)
+print(result2)
+print('*' * 30)
+print(result3)
 ###################################
 conn.commit()
 cur.close()
