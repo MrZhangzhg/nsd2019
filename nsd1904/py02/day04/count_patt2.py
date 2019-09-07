f83dc7cc0442118f0812b92a81a1acf603f77b17
MrZhangzhg <zhangzg@tedu.cn>
2019-09-07 09:31:31 +0800

    nsd1904 py02 day05 9:30

--- nsd1904/py02/day04/count_patt2.py
+++ nsd1904/py02/day04/count_patt2.py
@@ -1,11 +1,14 @@
 import re
 
 class CountPatt:
-    def count_patt(self, fname, patt):
+    def __init__(self, fname):
+        self.fname = fname
+
+    def count_patt(self, patt):
         cpatt = re.compile(patt)  # 为了效率，先编译模式
         result = {}  # 用于保存结果
 
-        with open(fname) as fobj:
+        with open(self.fname) as fobj:
             for line in fobj:
                 m = cpatt.search(line)
                 if m:  # 如果匹配到，匹配对象表示真；没匹配到，返回None，表示假
@@ -18,5 +21,12 @@ class CountPatt:
 if __name__ == '__main__':
     fname = 'access_log'
     ip = '^(\d+\.){3}\d+'  # 192.168.1.15   1234.56789.1.12345678
-    cp = CountPatt()
-    print(cp.count_patt(fname, ip))
+    br = 'Firefox|MSIE|Chrome'
+    os_patt = 'Linux|Windows|Android'
+    cp = CountPatt(fname)
+    print(cp.count_patt(ip))
+    print(cp.count_patt(br))
+    print(cp.count_patt(os_patt))
+    print('#' * 50)
+    cp1 = CountPatt('/etc/passwd')
+    print(cp1.count_patt('bash$|nologin$'))
