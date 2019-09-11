a46ffffb14034c4d6a28afffbe9ef5fb3d355d61
MrZhangzhg <zhangzg@tedu.cn>
2019-09-11 15:54:34 +0800

    nsd1904 devops day02 15:50

--- nsd1904/devops/day02/dingding.py
+++ nsd1904/devops/day02/dingding.py
@@ -5,11 +5,39 @@ import json
 # 按要求修改请求头
 headers = {'Content-Type': 'application/json;charset=utf-8'}
 # 在阿里巴巴钉钉开放平台上把数据格式复制过来
+# data = {
+#     "msgtype": "text",
+#     "text": {
+#         "content": "你好，nsd1904测试"
+#     },
+#     "at": {
+#         "atMobiles": [
+#             "156xxxx8827",
+#             "189xxxx8325"
+#         ],
+#         "isAtAll": False
+#     }
+# }
+
+# data = {
+#     "msgtype": "link",
+#     "link": {
+#         "text": "devops是development和opations的结合",
+#         "title": "nsd1904 python",
+#         "picUrl": "https://img01.sogoucdn.com/app/a/100520021/09e16c260f901c78c70df448a142efd6",
+#         "messageUrl": "http://fanyi.sogou.com"
+#     }
+# }
+
 data = {
-    "msgtype": "text",
-    "text": {
-        "content": "你好，nsd1904测试"
-    },
+     "msgtype": "markdown",
+     "markdown": {
+         "title":"杭州天气",
+         "text": "#### 杭州天气 @156xxxx8827\n" +
+                 "> 9度，西北风1级，空气良89，相对温度73%\n\n" +
+                 "> ![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)\n"  +
+                 "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n"
+     },
     "at": {
         "atMobiles": [
             "156xxxx8827",
@@ -17,7 +45,8 @@ data = {
         ],
         "isAtAll": False
     }
-}
+ }
+
 # url是你群聊机器人的webhook地址
 url = getpass.getpass()
 # 发送post请求
