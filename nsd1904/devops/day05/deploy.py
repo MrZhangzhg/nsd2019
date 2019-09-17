e2ebddfed0ddcdd3a7f847bcc28f6b020075abb5
MrZhangzhg <zhangzg@tedu.cn>
2019-09-17 17:07:50 +0800

    nsd1904 devops day05 17:10

--- nsd1904/devops/day05/deploy.py
+++ nsd1904/devops/day05/deploy.py
@@ -8,6 +8,7 @@
 import os
 import wget
 import requests
+import hashlib
 
 def has_new_ver(ver_fname, ver_url):
     "如果本地没有版本文件，或版本低都返回True"
@@ -27,6 +28,25 @@ def has_new_ver(ver_fname, ver_url):
 
 
 def check_file(app_fname, md5_url):
+    "本地文件的值和网上提供的值一致，返回True；否则返回False"
+    # 计算本地文件的md5值
+    m = hashlib.md5()
+
+    with open(app_fname, 'rb') as fobj:
+        while True:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+
+    # 取出网上的md5值
+    r = requests.get(md5_url)
+
+    # 比较
+    if m.hexdigest() == r.text.strip():
+        return True
+    else:
+        return False
 
 
 def deploy(app_fname):
@@ -52,6 +72,7 @@ if __name__ == '__main__':
     md5_url = app_url + '.md5'
     if not check_file(app_fname, md5_url):
         print('文件已损坏。')
+        os.remove(app_fname)
         exit(2)
 
     # 部署软件
