eb8bc5b6a0b235824752bcfc310776715a7d8b68
MrZhangzhg <zhangzg@tedu.cn>
2019-09-17 16:13:52 +0800

    nsd1904 devops day05 16:15

--- nsd1904/devops/day05/deploy.py
+++ nsd1904/devops/day05/deploy.py
@@ -7,29 +7,35 @@
 
 import wget
 
-def has_new_ver():
+def has_new_ver(ver_fname, ver_url):
 
 
-def check_file():
+def check_file(app_fname, md5_url):
 
 
-def deploy():
+def deploy(app_fname):
 
 
 if __name__ == '__main__':
+    ver_fname = '/var/www/deploy/live_ver'
+    ver_url = 'http://192.168.4.6/deploy/live_ver'
     # 如果没有新版本的软件，则退出
-    if not has_new_ver():
+    if not has_new_ver(ver_fname, ver_url):
         print('未发现新版本软件。')
         exit(1)
 
     # 如果有新版本软件，则下载
 
+
+
     # 如果下载的软件已损坏，则把它删除，并退出
-    if not check_file():
+    app_fname = ''
+    md5_url = ''
+    if not check_file(app_fname, md5_url):
         print('文件已损坏。')
         exit(2)
 
     # 部署软件
-    deploy()
+    deploy(app_fname)
 
     # 更新最新版本信息
