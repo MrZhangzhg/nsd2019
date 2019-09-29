fbf77912a855c1b54192f1619bf2c825aeebae69
MrZhangzhg <zhangzg@tedu.cn>
2019-09-29 11:56:42 +0800

    nsd1905 py01 day01 12:00

--- nsd1905/py01/day01/hello.py
+++ nsd1905/py01/day01/hello.py
@@ -1,3 +1,19 @@
 #!/root/nsd1905/bin/python
 
-print('hello world!')
+# print('hello world!')
+#
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+#
+# if 10 > 5: print('true')   # 没有语法错误，但是可读性不好，不推荐
+
+# print("hello world!")
+# print("hao", 123)  # 输出字符串和数字
+# print('Hao', 123, sep='---')   # 指定输出各项之间，用---分隔
+# print('Hello' + 'World')  # 字符串拼接，使用+
+
+uname = input('username: ')
+print(uname)
+
+
