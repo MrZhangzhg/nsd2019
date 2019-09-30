83171f667f8f7abfe430538c01e7660d5b2b8a2c
MrZhangzhg <zhangzg@tedu.cn>
2019-09-30 14:57:34 +0800

    nsd1905 py01 day02 15:00

---
+++ nsd1905/py01/day02/for1.py
@@ -0,0 +1,19 @@
+s1 = 'abc'
+alist = [10, 20, 30]
+atuple = ('bob', 'alice')
+adict = {'name': 'tom', 'age': 22}
+
+for ch in s1:
+    print(ch)
+
+for i in alist:
+    print(i)
+
+for name in atuple:
+    print(name)
+
+for key in adict:
+    print(key, adict[key])
+
+
+
