d26733453861114ea3ce914975a3d6e8e172af65
MrZhangzhg <zhangzg@tedu.cn>
2019-10-09 16:10:24 +0800

    nsd1905 py01 day04 16:10

--- nsd1905/py01/day04/day04.py
+++ nsd1905/py01/day04/day04.py
@@ -1,5 +1,20 @@
-import shutil
+# import shutil
+#
+# shutil.copyfileobj()
+# shutil.copyfile()
+# shutil.copy()
 
-shutil.copyfileobj()
-shutil.copyfile()
-shutil.copy()
+from random import randint
+
+nums = [randint(1, 100) for i in range(10)]
+print(nums)
+
+# for i in range(len(nums)):
+#     print(i, nums[i])
+
+print(list(enumerate(nums)))
+for data in enumerate(nums):
+    print(data)
+
+for ind, num in enumerate(nums):
+    print(ind, num)
