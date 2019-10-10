ca05ceab4a4fc518b88fcb19070e6a54527aa577
MrZhangzhg <zhangzg@tedu.cn>
2019-10-10 11:46:26 +0800

    nsd1905 py01 day05  11：30

--- nsd1905/py01/day05/stack.py
+++ nsd1905/py01/day05/stack.py
@@ -8,7 +8,26 @@ def view_it():
     print('view')
 
 def show_menu():
+    prompt = '''(0) 压栈
+(1) 出栈
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): '''
+    while 1:
+        choice = input(prompt).strip()  # 删除用户输出的额外的空格
+        if choice not in ['0', '1', '2', '3']:
+            print('无效输入，请重试。')
+            continue
 
+        if choice == '0':
+            push_it()
+        elif choice == '1':
+            pop_it()
+        elif choice == '2':
+            view_it()
+        else:
+            print('\nBye-bye')
+            break
 
 if __name__ == '__main__':
     show_menu()
