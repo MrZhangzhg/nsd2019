22c99bbc053ec4b1814f2584aaf8ee82a8254efe
MrZhangzhg <zhangzg@tedu.cn>
2019-10-12 10:29:29 +0800

    nsd1905 py02 day01 10:35

--- nsd1905/py02/day01/cut_file.py
+++ nsd1905/py02/day01/cut_file.py
@@ -7,3 +7,18 @@
 # 2019-05-15 12:35:03 gggg
 # 2019-05-15 13:13:24 hhhh
 
+import time
+
+t9 = time.strptime('2019-05-15 09:00:00', '%Y-%m-%d %H:%M:%S')
+t12 = time.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
+
+with open('mylog.txt') as fobj:
+    for line in fobj:
+        t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+        # if t9 < t < t12:
+        #     print(line, end='')
+        if t > t12:
+            break
+        if t > t9:
+            print(line, end='')
+        
