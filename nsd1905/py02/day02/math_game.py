2ca00492f157868107085334cec2776d188bd42f
MrZhangzhg <zhangzg@tedu.cn>
2019-10-14 11:49:45 +0800

    nsd1905 py02 day02 11:50

--- nsd1905/py02/day02/math_game.py
+++ nsd1905/py02/day02/math_game.py
@@ -11,15 +11,32 @@
 # Continue(y/n)? n
 # Bye-bye
 
+from random import randint, choice
+
 def exam():
-    print('exam')
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列
+    op = choice('+-')  # 随机选+-
+    # 算出正确答案
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+
+    # 用户答题
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    answer = int(input(prompt))
+    if answer == result:
+        print('太棒了！！！')
+    else:
+        print('不对哟！')
 
 def main():
     while 1:
         exam()
         try:
             yn = input('Continue(y/n)? ').strip()[0]  # 取出第一个字符
-        except IndexError:
+        except IndexError:  # 空白字符导致的异常，认为用户要继续
             continue
         except (KeyboardInterrupt, EOFError):
             yn = 'n'
