9d6b1965d269c30ccb4000ac3c8ee1ed0ed261bd
MrZhangzhg <zhangzg@tedu.cn>
2019-10-14 14:54:25 +0800

    nsd1905 py02 day02 14:50

--- nsd1905/py02/day02/day02.py
+++ nsd1905/py02/day02/day02.py
@@ -1,9 +1,31 @@
-def func1():
-    print('in func1')
-    func2()
+# def func1():
+#     print('in func1')
+#     func2()
+#
+# def func2():
+#     print('in func2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('in func2')
+from random import randint
+
+def func1(x):
+    return True if x % 2 == 1 else False
 
 if __name__ == '__main__':
-    func1()
+    nums = [randint(1, 100) for i in range(10)]
+    print(nums)
+    result = filter(func1, nums)
+    print(list(result))
+    result2 = filter(lambda x: True if x % 2 == 1 else False, nums)
+    print(list(result2))
+
+
+
+
+
+
+
+
+
