ebdb7274d803ec909324fac6108cfd6919edfccc
MrZhangzhg <zhangzg@tedu.cn>
2019-10-16 09:35:50 +0800

    nsd1905 py02 day04 9:40

---
+++ nsd1905/py02/day04/count_patt.py
@@ -0,0 +1,23 @@
+import re
+
+def count_patt(fname, patt):
+    patt_dict = {}
+    cpatt = re.compile(patt)  # 编译模式，提升效率
+
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:  # 如果匹配到，则为真，匹配不到是None，为假
+                key = m.group()
+                patt_dict[key] = patt_dict.get(key, 0) + 1
+
+    return patt_dict
+
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = '^(\d+\.){3}\d+'  # 1234.56789.10.8  1.23.123.5
+    br = 'Firefox|MSIE|Chrome'
+    result1 = count_patt(fname, ip)
+    result2 = count_patt(fname, br)
+    print(result1)
+    print(result2)
