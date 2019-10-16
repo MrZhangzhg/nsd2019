644d10edd812c626772b78dce45d5b96473687a5
MrZhangzhg <zhangzg@tedu.cn>
2019-10-16 14:15:54 +0800

    nsd1905 py02 day04 14:20

--- nsd1905/py02/day04/py_mysql.py
+++ nsd1905/py02/day04/py_mysql.py
@@ -14,9 +14,29 @@ conn = pymysql.connect(
 cur = conn.cursor()
 
 # 编写sql语句
-
+create_dep = '''CREATE TABLE departments(
+dep_id INT, dep_name VARCHAR(20),
+PRIMARY KEY(dep_id)
+)'''
+create_emp = '''CREATE TABLE employees(
+emp_id INT, emp_name VARCHAR(20), birth_date DATE, email VARCHAR(50),
+dep_id INT,
+PRIMARY KEY(emp_id),
+FOREIGN KEY(dep_id) REFERENCES departments(dep_id)
+)'''
+create_sal = '''CREATE TABLE salary(
+id INT, date DATE, emp_id INT, basic INT, awards INT,
+PRIMARY KEY(id),
+FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
+)'''
 
 # 执行sql语句
+cur.execute(create_dep)
+cur.execute(create_emp)
+cur.execute(create_sal)
+
+# 对数据库有改动，需要确认
+conn.commit()
 
 # 关闭
 cur.close()
