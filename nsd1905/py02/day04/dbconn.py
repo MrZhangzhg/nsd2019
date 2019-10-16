c8e56c35539b7a242a3073c0f78193c30e21e3da
MrZhangzhg <zhangzg@tedu.cn>
2019-10-16 16:13:38 +0800

    nsd1905 py02 day04 16:10

--- nsd1905/py02/day04/dbconn.py
+++ nsd1905/py02/day04/dbconn.py
@@ -1,9 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-
 # 创建到数据库的引擎
-
 engine = create_engine(
     # mysql+pymysql://用户:密码@服务器/数据库?选项
     'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1905?charset=utf8',
@@ -14,9 +12,17 @@ engine = create_engine(
 Base = declarative_base()
 
 class Departments(Base):
-    __tablename__ = 'Departments'  # 指定类对应的表
+    __tablename__ = 'departments'  # 指定类对应的表
     dep_id = Column(Integer, primary_key=True)
     dep_name = Column(String(20), unique=True)
+
+class Employees(Base):
+    __tablename__ = 'employees'
+    emp_id = Column(Integer, primary_key=True)
+    emp_name = Column(String(20))
+    birth_date = Column(Date)
+    email = Column(String(50))
+    dep_id = Column(Integer, ForeignKey('departments.dep_id'))
 
 if __name__ == '__main__':
     # 如果库中没有表则创建，有的话只是进行关联，不会再创建一遍
