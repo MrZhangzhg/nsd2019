f694862bb2357a34b05b0d624fb6675e0e25ec60
MrZhangzhg <zhangzg@tedu.cn>
2019-10-23 17:56:06 +0800

    18:00

--- nsd1905/devops/day05/deploy.py
+++ nsd1905/devops/day05/deploy.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import wget
+import hashlib
+import tarfile
 
 
 def has_new_ver(ver_fname, ver_url):
@@ -24,10 +26,41 @@ def has_new_ver(ver_fname, ver_url):
 
 def check_app(app_fname, md5_url):
     '用于校验软件包是否完好，完好返回True'
+    # 计算文件的md5值
+    m = hashlib.md5()
+    with open(app_fname, 'rb') as fobj:
+        while 1:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
 
+    # 在网上取出公布的md5值
+    r = requests.get(md5_url)
 
-def deploy():
+    # 判断两个值是否相等
+    if m.hexdigest() == r.text.strip():
+        return True
+    else:
+        return False
+
+def deploy(app_fname, deploy_dir):
     '部署软件包到web服务器'
+    # 解压到部署目录
+    tar = tarfile.open(app_fname)
+    tar.extractall(path=deploy_dir)
+    tar.close()
+
+    # 拼接出解压目录的绝对路径
+    dest = app_fname.split('/')[-1]
+    dest = dest.replace('.tar.gz', '')
+    dest = os.path.join(deploy_dir, dest)
+
+    # 创建链接
+    link = '/var/www/html/nsd1905'
+    if os.path.exists(link):  # 如果链接文件已存在，先删除它
+        os.remove(link)
+    os.symlink(dest, link)
 
 
 if __name__ == '__main__':
@@ -50,11 +83,15 @@ if __name__ == '__main__':
     app_fname = app_url.split('/')[-1]
     app_fname = os.path.join(download_dir, app_fname)
     if not check_app(app_fname, md5_url):
+        os.remove(app_fname)
         print('文件已损坏。')
         exit(2)
 
     # 部署软件包
-    deploy()
+    deploy_dir = '/var/www/deploy'
+    deploy(app_fname, deploy_dir)
 
     # 更新本地软件版本文件
-
+    if os.path.exists(ver_fname):
+        os.remove(ver_fname)
+    wget.download(ver_url, ver_fname)
