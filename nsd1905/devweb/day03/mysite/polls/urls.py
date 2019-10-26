4c6a1e7a8c4ec1102a8f10d7d553e8dc1a35f367
MrZhangzhg <zhangzg@tedu.cn>
2019-10-26 14:12:39 +0800

    nsd1905 devweb day03 14:10

--- nsd1905/devweb/day03/mysite/polls/urls.py
+++ nsd1905/devweb/day03/mysite/polls/urls.py
@@ -1,3 +1,8 @@
 from django.conf.urls import url
+# from polls import views
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    # 访问首页时，使用views.index函数响应，该url的名字是index
+    url(r'^$', views.index, name='index'),
+]
--- nsd1905/devweb/day03/mysite/polls/views.py
+++ nsd1905/devweb/day03/mysite/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # 用户发起请求，请求的内容将会作为函数的第一个参数，所以函数至少需要有一个参数
+    return render(request, 'index.html')
+
