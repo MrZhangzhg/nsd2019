6ac8bb1aa072c6cbfe197b05f9ba58665eaf8756
MrZhangzhg <zhangzg@tedu.cn>
2019-11-01 09:55:32 +0800

    nsd1906 py01 day02 9:50

--- nsd1906/py01/day02/if1.py
+++ nsd1906/py01/day02/if1.py
@@ -1,25 +1,41 @@
-py_str = 'Python'
+# py_str = 'Python'
+#
+# if 'th' in py_str:
+#     print('th在Python中')
+#
+# if -0.0:
+#     print('值为0的数字为假')
+#
+# if ' ':
+#     print('空格也是一个字符，为真')
+#
+# if []:
+#     print('空列表为假')
+#
+# if (1, 2):
+#     print('非空元组，为真')
+#
+# if {'name': 'bob'}:
+#     print('非空字典，为真')
+#
+# if not {}:
+#     print('空字典为假，取反为真')
+#
+# if not None:
+#     print('None表示空对象，类似于mysql中的null，为假，取反为真')
 
-if 'th' in py_str:
-    print('th在Python中')
+a = 10
+b = 20
+if a < b:
+    s1 = a
+else:
+    s1 = b
+print('s1=%s' % s1)
 
-if -0.0:
-    print('值为0的数字为假')
+s2 = a if a < b else b
+print('s2=%s' % s2)
 
-if ' ':
-    print('空格也是一个字符，为真')
 
-if []:
-    print('空列表为假')
 
-if (1, 2):
-    print('非空元组，为真')
 
-if {'name': 'bob'}:
-    print('非空字典，为真')
 
-if not {}:
-    print('空字典为假，取反为真')
-
-if not None:
-    print('None表示空对象，类似于mysql中的null，为假，取反为真')
