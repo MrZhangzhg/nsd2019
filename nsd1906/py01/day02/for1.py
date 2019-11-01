9bc1b900d674a02ff1e46af9cc2bcaa5e103da4e
MrZhangzhg <zhangzg@tedu.cn>
2019-11-01 16:24:59 +0800

    nsd1906 py01 day02 16:30

---
+++ nsd1906/py01/day02/for1.py
@@ -0,0 +1,22 @@
+s1 = 'hello'
+nums = [10, 20, 15, 80]
+names = ('tom', 'jerry')
+user = {'name': 'bob', 'age': 20}
+
+for ch in s1:
+    print(ch)
+
+print('*' * 50)
+for i in nums:
+    print(i)
+
+print('*' * 50)
+for name in names:
+    print(name)
+
+print('*' * 50)
+for key in user:
+    print(key, user[key])
+
+
+
