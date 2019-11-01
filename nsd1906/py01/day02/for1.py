c41413a8c8330497fe49d558ec64c63d127f6e37
MrZhangzhg <zhangzg@tedu.cn>
2019-11-01 17:38:15 +0800

    nsd1906 py01 day02 17:40

--- nsd1906/py01/day02/for1.py
+++ nsd1906/py01/day02/for1.py
@@ -2,21 +2,34 @@ s1 = 'hello'
 nums = [10, 20, 15, 80]
 names = ('tom', 'jerry')
 user = {'name': 'bob', 'age': 20}
+#
+# for ch in s1:
+#     print(ch)
+#
+# print('*' * 50)
+# for i in nums:
+#     print(i)
+#
+# print('*' * 50)
+# for name in names:
+#     print(name)
+#
+# print('*' * 50)
+# for key in user:
+#     print(key, user[key])
 
-for ch in s1:
-    print(ch)
 
-print('*' * 50)
-for i in nums:
+# for循环常与range连用
+print(range(10))  # range只是一个对象，潜在可以生成很多数
+# range没有给起始数字，从0开始。结束数字不包含
+for i in range(10):
     print(i)
 
-print('*' * 50)
-for name in names:
-    print(name)
+# 将range转换成列表再打印
+print(list(range(10)))
 
-print('*' * 50)
-for key in user:
-    print(key, user[key])
+# 打印6到10的列表
+print(list(range(6, 11)))
 
-
-
+print(list(range(1, 10, 2)))
+print(list(range(10, 0, -1)))
