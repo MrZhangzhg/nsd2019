df34f08633b2da6fe0c3ee1caeb5118581fdfeb1
MrZhangzhg <zhangzg@tedu.cn>
2019-11-04 14:36:02 +0800

    nsd1906 py01 day04 14:30

--- nsd1906/py01/day04/mkfile.py
+++ nsd1906/py01/day04/mkfile.py
@@ -1,5 +1,15 @@
+import os
+
 def get_fname():
     '返回一个文件名字符串'
+    while 1:
+        fname = input('文件名: ')
+        if not os.path.exists(fname):
+            break
+
+        print('文件已存在，请重试。')
+
+    return fname
 
 def get_content():
     '返回文件内容的字符串列表'
