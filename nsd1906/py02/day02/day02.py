491c6a59c5c1ce9e8ec035e0aa94d657ed234b89
MrZhangzhg <zhangzg@tedu.cn>
2019-11-09 15:20:28 +0800

    nsd1906 py02 day02 15:25

--- nsd1906/py02/day02/day02.py
+++ nsd1906/py02/day02/day02.py
@@ -14,14 +14,20 @@ def func1(x):
     return x % 2   # 结果只有1和0两种情况，1为真，0为假
     # return True if x % 2 == 1 else False
 
+def func2(x):
+    return x * 2
+
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    result = filter(func1, nums)
-    print(list(result))
-    result2 = filter(lambda x: x % 2, nums)
-    print(list(result2))
-
+    # result = filter(func1, nums)
+    # print(list(result))
+    # result2 = filter(lambda x: x % 2, nums)
+    # print(list(result2))
+    result3 = map(func2, nums)
+    print(list(result3))
+    result4 = map(lambda x: x * 2, nums)
+    print(list(result4))
 
 
 
