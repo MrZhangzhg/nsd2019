f0a57c48277b7046998c0a0d32c423d93e89f8e4
MrZhangzhg <zhangzg@tedu.cn>
2019-11-12 17:27:02 +0800

    nsd1906 py02 day04 17:20

--- nsd1906/py02/day04/dbconn.py
+++ nsd1906/py02/day04/dbconn.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # 创建到数据库的连接引擎
+
 engine = create_engine(
     # mysql+pymysql://用户:密码@服务器/数据库?参数
     'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1906?charset=utf8',
@@ -10,6 +12,8 @@ engine = create_engine(
 )
 # 创建基类
 Base = declarative_base()
+# 创建会话类
+Session = sessionmaker(bind=engine)
 
 # 创建实体类，必须继承基类
 class Departments(Base):
@@ -25,6 +29,13 @@ class Employees(Base):
     email = Column(String(50))
     dep_id = Column(Integer, ForeignKey('departments.dep_id'))
 
+class Salary(Base):
+    __tablename__ = 'salary'
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    emp_id = Column(Integer, ForeignKey('employees.emp_id'))
+    basic = Column(Integer)
+    awards = Column(Integer)
 
 if __name__ == '__main__':
     # 如果库中不存在对应的表则创建；存在就不创建了
