10a6dd5b4e273cff8ea4e1eeb6a6ab7e83566e5e
MrZhangzhg <zhangzg@tedu.cn>
2019-11-22 14:35:41 +0800

    nsd1906 devweb day03 14:40

--- nsd1906/devweb/day03/mysite/polls/urls.py
+++ nsd1906/devweb/day03/mysite/polls/urls.py
@@ -1,3 +1,10 @@
 from django.conf.urls import url
+# from polls import views   # 也可以用下面的形式
+from . import views   # 从当前目录(包)导入views模块
 
-urlpatterns = []
+urlpatterns = [
+    # url从http://x.x.x.x/polls/后面开始匹配
+    # 访问投票首页时，使用views.index函数响应
+    # 为该url(http://x.x.x.x/polls/)起名为index
+    url(r'^$', views.index, name='index'),
+]
