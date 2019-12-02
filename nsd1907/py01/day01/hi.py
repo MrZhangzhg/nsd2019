3f2462bacdc3e35205110603ab559af337c81e87
MrZhangzhg <zhangzg@tedu.cn>
2019-12-02 11:43:56 +0800

    nsd1907 py01 day01 11:45

---
+++ nsd1907/py01/day01/hi.py
@@ -0,0 +1,29 @@
+# print("Hello World!")
+#
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+#
+# s = 'Hello World!' + \
+#     'Hello tedu'
+#
+# a = 3; b = 4
+# c = 5
+# d = 6
+
+# 打印一行字符串
+print('Hello World!')
+
+# 字符串可以使用+进行拼接，拼接后再打印
+print('Hello' + 'World')
+
+# print打印多项时，用逗号分开各项
+print('Hao', 123)
+
+# 也可以通过sep指定分隔符
+print('Hao', 123, 'abc', 456)
+print('Hao', 123, 'abc', 456, sep='***')
+
+
+
+
