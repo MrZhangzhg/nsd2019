b1324b29b34ca864190a420cf014a5ce01d74797
MrZhangzhg <zhangzg@tedu.cn>
2019-12-03 16:45:46 +0800

    nsd1907 py01 day02 16:40

---
+++ nsd1907/py01/day02/for1.py
@@ -0,0 +1,21 @@
+s = 'python'
+for ch in s:
+    print(ch)
+
+print('*' * 30)
+
+users = ['tom', 'jerry', 'bob']
+for name in users:
+    print(name)
+
+print('*' * 30)
+
+nums = (10, 20, 30)
+for i in nums:
+    print(i)
+
+print('*' * 30)
+
+adict = {'name': 'tom', 'age': 20}
+for key in adict:
+    print(key, adict[key])
