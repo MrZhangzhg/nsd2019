3ec2d5eec37f5f70a45c624187001080ed3240e4
MrZhangzhg <zhangzg@tedu.cn>
2019-12-03 14:08:31 +0800

    nsd1907 py01 day02 14:10

---
+++ nsd1907/py01/day02/game3.py
@@ -0,0 +1,20 @@
+import random
+
+choices = ['石头', '剪刀', '布']
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+prompt = """(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): """
+
+computer = random.choice(choices)
+ind = int(input(prompt))
+player = choices[ind]
+
+print("Your choice: %s, computer's choice: %s" % (player, computer))
+if player == computer:
+    print('\033[32;1m平局\033[0m')
+elif [player, computer] in win_list:
+    print('\033[31;1mYou WIN!!!\033[0m')
+else:
+    print('\033[31;1mYou LOSE!!!\033[0m')
