040048c1e5faee8aefafed426a8c872ad0242813
MrZhangzhg <zhangzg@tedu.cn>
2019-12-03 14:49:35 +0800

    nsd1907 py01 day02 14:50

---
+++ nsd1907/py01/day02/game4.py
@@ -0,0 +1,25 @@
+import random
+
+choices = ['石头', '剪刀', '布']
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+prompt = """(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): """
+pwin = 0
+cwin = 0
+
+while pwin < 2 and cwin < 2:  # 人机都没有赢够两次，就要继续
+    computer = random.choice(choices)
+    ind = int(input(prompt))
+    player = choices[ind]
+
+    print("Your choice: %s, computer's choice: %s" % (player, computer))
+    if player == computer:
+        print('\033[32;1m平局\033[0m')
+    elif [player, computer] in win_list:
+        pwin += 1
+        print('\033[31;1mYou WIN!!!\033[0m')
+    else:
+        cwin += 1
+        print('\033[31;1mYou LOSE!!!\033[0m')
---
+++ nsd1907/py01/day02/while1.py
@@ -0,0 +1,8 @@
+result = 0    # 创建变量用于保存加法的结果
+counter = 1   # 创建计数器，用于累加到result中
+
+while counter < 101:
+    result += counter
+    counter += 1
+
+print(result)
