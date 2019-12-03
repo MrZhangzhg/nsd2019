8769f256ec0ce542e298ab770b843fb571eabbf1
MrZhangzhg <zhangzg@tedu.cn>
2019-12-03 11:35:21 +0800

    nsd1907 py01 day02 11:40

---
+++ nsd1907/py01/day02/game.py
@@ -0,0 +1,28 @@
+import random
+
+choices = ['石头', '剪刀', '布']
+computer = random.choice(choices)
+player = input('请出拳(石头/剪刀/布): ')
+
+print("Your choice: %s, computer's choice: %s" % (player, computer))
+if player == '石头':
+    if computer == '石头':
+        print('平局')
+    elif computer == '剪刀':
+        print('You WIN!!!')
+    else:
+        print('You LOSE!!!')
+elif player == '剪刀':
+    if computer == '石头':
+        print('You LOSE!!!')
+    elif computer == '剪刀':
+        print('平局')
+    else:
+        print('You WIN!!!')
+else:
+    if computer == '石头':
+        print('You WIN!!!')
+    elif computer == '剪刀':
+        print('You LOSE!!!')
+    else:
+        print('平局')
