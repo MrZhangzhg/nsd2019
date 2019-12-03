7ed64916d110e2dd0716cb16620a9302067a111c
MrZhangzhg <zhangzg@tedu.cn>
2019-12-03 09:35:05 +0800

    nsd1907 py01 day02 9:40

---
+++ nsd1907/py01/day02/day02.py
@@ -0,0 +1,31 @@
+if 3 > 0:
+    print('3比0大')
+
+if 'to' in 'python':
+    print('to在python中')
+else:
+    print('to不在python中')
+
+if -0.0:
+    print('任何值为0的数字都是False')
+
+if 0.01:
+    print('非0为True')
+
+if ' ':
+    print('空格也是一个字符，非空为True')
+
+if [10, 20]:
+    print('非空列表为True')
+
+if ():
+    print('空元组为False')
+
+if {'name': 'tom'}:
+    print('非空字典为True')
+
+if not None:
+    print('None为False，取反为True')
+
+if not 0:
+    print('0为False，取反为True')
