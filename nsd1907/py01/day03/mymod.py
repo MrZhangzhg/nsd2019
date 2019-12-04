6c96c13b01fce7482433da6c91016613f5a5b886
MrZhangzhg <zhangzg@tedu.cn>
2019-12-04 16:54:09 +0800

    nsd1907 py01 day03 16:50

---
+++ nsd1907/py01/day03/mymod.py
@@ -0,0 +1,13 @@
+# import star   # 常用
+# print(star.hi)
+# star.pstar()
+
+# from random import choice, randint   # 常用
+# print(choice('abcd'))
+# print(randint(1, 100))
+
+# import sys, getpass   # 不常用
+
+import getpass as gp    # 导入模块时，给模块取别名，不常用
+upass = gp.getpass()
+print(upass)
