bc273300868a938d203de3c0fef6eee91e1b6544
MrZhangzhg <zhangzg@tedu.cn>
2019-12-05 10:52:25 +0800

    nsd1907 py01 day04 10:50

---
+++ nsd1907/py01/day04/ping.py
@@ -0,0 +1,14 @@
+import sys
+import subprocess
+
+def ping(host):
+    result = subprocess.run(
+        'ping -c2 %s &> /dev/null' % host, shell=True
+    )
+    if result.returncode == 0:
+        print('%s:up' % host)
+    else:
+        print('%s:down' % host)
+
+if __name__ == '__main__':
+    ping(sys.argv[1])
