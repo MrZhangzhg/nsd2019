4ee9b6187b1a37fc82647dbdb2502e61ce788ec8
MrZhangzhg <zhangzg@tedu.cn>
2019-12-05 14:29:36 +0800

    nsd1907 py01 day04 14:30

--- nsd1907/py01/day04/mkfile.py
+++ nsd1907/py01/day04/mkfile.py
@@ -1,5 +1,14 @@
+import os
+
 def get_fname():
     '用于获取文件名'
+    while 1:
+        fname = input('文件名: ')
+        if not os.path.exists(fname):
+            break
+        print('文件已存在，请重试。')
+
+    return fname
 
 def get_content():
     '用于获取内容'
