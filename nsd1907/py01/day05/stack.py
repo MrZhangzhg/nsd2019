3911eae33cb554af73406557422827d2ac590f48
MrZhangzhg <zhangzg@tedu.cn>
2019-12-06 10:57:50 +0800

    nsd1907 py01 day05 10:55

--- nsd1907/py01/day05/stack.py
+++ nsd1907/py01/day05/stack.py
@@ -1,13 +1,34 @@
 def push_it():
-
+    print('push')
 
 def pop_it():
-
+    print('pop')
 
 def view_it():
-
+    print('view')
 
 def show_menu():
+    prompt = """(0) 压栈
+(1) 出栈
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
+
+    while 1:
+        choice = input(prompt).strip()  # 去除字符串两端空白字符
+        if choice not in ['0', '1', '2', '3']:
+            print('无效的输入，请重试。')
+            continue
+
+        if choice == '0':
+            push_it()
+        elif choice == '1':
+            pop_it()
+        elif choice == '2':
+            view_it()
+        else:
+            print('Bye-bye')
+            break
 
 
 if __name__ == '__main__':
