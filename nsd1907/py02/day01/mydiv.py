9b3d53b47607ffeb5330072f8ce2106c30ce7d28
MrZhangzhg <zhangzg@tedu.cn>
2019-12-09 14:14:00 +0800

    nsd1907 py02 day01 14:18

--- nsd1907/py02/day01/mydiv.py
+++ nsd1907/py02/day01/mydiv.py
@@ -22,12 +22,41 @@
 # except (KeyboardInterrupt, EOFError):
 #     print('\nBye-bye')
 
+# try:
+#     num = int(input('number: '))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError) as e:  # 将异常保存到变量e
+#     print('无效的输入:', e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+# try:
+#     num = int(input('number: '))
+#     result = 100 / num
+# except (ValueError, ZeroDivisionError) as e:  # 将异常保存到变量e
+#     print('无效的输入:', e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+#     exit(101)  # 程序遇到exit将会彻底结束
+# else:
+#     print(result)
+#
+# print('Done')
+
 try:
     num = int(input('number: '))
     result = 100 / num
-    print(result)
-    print('Done')
 except (ValueError, ZeroDivisionError) as e:  # 将异常保存到变量e
     print('无效的输入:', e)
 except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
+    exit(101)  # 程序遇到exit将会彻底结束
+else:
+    print(result)
+finally:
+    print('一定要执行的语句')
+
+print('Done')
+
