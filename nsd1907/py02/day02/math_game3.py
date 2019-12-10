1c631a433ae2d33fc32eb4afbed2e9cb1fcd7fad
MrZhangzhg <zhangzg@tedu.cn>
2019-12-10 15:38:47 +0800

    nsd1907 py02 day02 15:45

---
+++ nsd1907/py02/day02/math_game3.py
@@ -0,0 +1,54 @@
+from random import choice, randint
+
+def exam():
+    '出题测试'
+    cmds = {'+': lambda x, y: x + y, '-': lambda x, y: x - y}
+    # 随机取出100以内的两个随机数字
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列
+
+    # 随机取出加减法
+    op = choice('+-')
+
+    # 算出正确答案
+    # result = cmds[op](nums[0], nums[1])
+    result = cmds[op](*nums)
+
+    # 用户作答
+    counter = 0
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:  # 不指定具体的异常，可以捕获全部异常，不推荐
+            print()
+            continue
+
+        if answer == result:
+            print('Very Good!!!')
+            break
+        else:
+            print('Wrong Anwswer!!!')
+        counter += 1
+    else:
+        print('\033[31;1m%s%s\033[0m' % (prompt, result))
+
+
+def main():
+    while 1:
+        exam()
+        # 将用户输入信息的额外空白字符删除后，取出第一个字符
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
+        if yn in 'nN':
+            print('\nBye-bye')
+            break
+
+if __name__ == '__main__':
+    main()
