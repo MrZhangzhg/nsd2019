588c013099d184ffdb598d1afd0570542d683831
MrZhangzhg <zhangzg@tedu.cn>
2019-12-10 16:04:57 +0800

    nsd1907 py02 day02 16:05

--- nsd1907/py02/day02/day02.py
+++ nsd1907/py02/day02/day02.py
@@ -9,21 +9,32 @@
 #     func1()
 
 
+# from random import randint
+#
+# def func1(x):
+#     return True if x % 2 == 0 else False
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     result = filter(func1, nums)
+#     result2 = filter(lambda x: True if x % 2 == 0 else False, nums)
+#     print(list(result))
+#     print(list(result2))
+
+
 from random import randint
 
-def func1(x):
-    return True if x % 2 == 0 else False
+def func2(x):
+    return x * 2 + 1
 
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    result = filter(func1, nums)
-    result2 = filter(lambda x: True if x % 2 == 0 else False, nums)
+    result = map(func2, nums)
+    result2 = map(lambda x: x * 2 + 1, nums)
     print(list(result))
     print(list(result2))
-
-
-
 
 
 
