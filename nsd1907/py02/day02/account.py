09a2bff82579677eb1d8e97eb35db1b6ea12b19c
MrZhangzhg <zhangzg@tedu.cn>
2019-12-10 09:55:18 +0800

    nsd1907 py02 day02 10:05

--- nsd1907/py02/day02/account.py
+++ nsd1907/py02/day02/account.py
@@ -1,11 +1,31 @@
+import os
+import pickle
+from time import strftime
 
-def save():
+
+def save(fname):
     '记录收入'
+    date = strftime('%Y-%m-%d')
+    amount = int(input('金额: '))
+    comment = input('备注: ')
 
-def cost():
+    # 在fname文件中取出最新余额，加上当前收入金额，即为最新余额
+    with open(fname, 'rb') as fobj:
+        records = pickle.load(fobj)
+
+    balance = records[-1][-2] + amount
+
+    # 将最新记录添加到文件中
+    record = [date, amount, 0, balance, comment]
+    records.append(record)
+    with open(fname, 'wb') as fobj:
+        pickle.dump(records, fobj)
+
+
+def cost(fname):
     '记录支出'
 
-def query():
+def query(fname):
     '查询'
 
 def show_menu():
@@ -15,6 +35,14 @@ def show_menu():
 (2) 查询
 (3) 退出
 请选择(0/1/2/3): """
+    fname = 'account.data'
+    init_data = [
+        ['2019-12-10', 0, 0, 10000, 'init data']
+    ]
+    # 如果文件不存在，则创建并写入初始化数据
+    if not os.path.exists(fname):
+        with open(fname, 'wb') as fobj:
+            pickle.dump(init_data, fobj)
 
     while 1:
         choice = input(prompt).strip()
@@ -26,7 +54,7 @@ def show_menu():
             print('\nBye-bye')
             break
 
-        cmds[choice]()
+        cmds[choice](fname)
 
 if __name__ == '__main__':
     show_menu()
