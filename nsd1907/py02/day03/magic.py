0245ebc49956111f990f776613093fa86768f540
MrZhangzhg <zhangzg@tedu.cn>
2019-12-11 18:05:55 +0800

    nsd1907 py02 day03 18:00

--- nsd1907/py02/day03/magic.py
+++ nsd1907/py02/day03/magic.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        # 打印实例时，执行此方法
+        # 打印实例时，执行此方法，必须返回字符串
         return '《%s》' % self.title
 
     def __call__(self):
@@ -14,4 +14,3 @@ if __name__ == '__main__':
     pybook = Book('Python核心编程', '韦斯利')  # 调用__init__
     print(pybook)  # 调用__str__方法
     pybook()   # 调用__call__方法
-
