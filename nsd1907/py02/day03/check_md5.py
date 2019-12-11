3520176f93db874f72b2ee98bcdd35b4855e763a
MrZhangzhg <zhangzg@tedu.cn>
2019-12-11 10:27:40 +0800

    nsd1907 py02 day03 10:35

---
+++ nsd1907/py02/day03/check_md5.py
@@ -0,0 +1,18 @@
+import hashlib
+import sys
+
+def check_md5(fname):
+    m = hashlib.md5()
+
+    with open(fname, 'rb') as fobj:
+        while 1:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+
+    return m.hexdigest()
+
+
+if __name__ == '__main__':
+    print(check_md5(sys.argv[1]))
