f0724a1392227e9bd9ef31c8c7812482693710d2
MrZhangzhg <zhangzg@tedu.cn>
2019-12-12 16:04:13 +0800

    nsd1907 py02 day04 16:01

--- nsd1907/py02/day04/dbconn.py
+++ nsd1907/py02/day04/dbconn.py
@@ -1,3 +1,24 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+# 创建到mysql数据库的连接引擎
+engine = create_engine(
+    # mysql+pymysql://用户:密码@主机/库?参数
+    'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1907?charset=utf8',
+    encoding='utf8',
+    # echo=True  # 在屏幕上打印日志
+)
 
+# 创建实体类的基类
+Base = declarative_base()
 
+# 创建实体类，也就是创建表对应的类
+class Departments(Base):
+    __tablename__ = 'departments'  # 声明类对应的表
+    # 每个字段都要用Column来定义
+    dep_id = Column(Integer, primary_key=True)  # 主键
+    dep_name = Column(String(20), unique=True)  # 名称必须唯一
+
+if __name__ == '__main__':
+    # 如果库中无表则创建，有表将不会执行
+    Base.metadata.create_all(engine)
