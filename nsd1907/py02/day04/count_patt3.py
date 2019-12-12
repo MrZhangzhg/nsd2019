83d8d2049e03e43c228424359ffbb4d0f19d257d
MrZhangzhg <zhangzg@tedu.cn>
2019-12-12 10:58:02 +0800

    nsd1907 py02 day04 10:50

--- nsd1907/py02/day04/count_patt3.py
+++ nsd1907/py02/day04/count_patt3.py
@@ -1,11 +1,14 @@
 import re
 
 class CountPatt:
-    def count_patt(self, fname, patt):
+    def __init__(self, fname):
+        self.fname = fname
+
+    def count_patt(self, patt):
         cpatt = re.compile(patt)  # 为了更好的执行效率，把模式编译
         patt_dict = {}  # 把结果保存到字典
 
-        with open(fname) as fobj:
+        with open(self.fname) as fobj:
             for line in fobj:
                 m = cpatt.search(line)  # 在一行中匹配模式
                 if m:  # 如果m不是None，非空为真
@@ -18,10 +21,10 @@ if __name__ == '__main__':
     ip = '^(\d+\.){3}\d+'
     br = 'Chrome|MSIE|Firefox'
     wl = 'Windows|Linux'
-    cp = CountPatt()
-    result = cp.count_patt(fname, ip)
-    result2 = cp.count_patt(fname, br)
-    result3 = cp.count_patt(fname, wl)
+    cp = CountPatt(fname)
+    result = cp.count_patt(ip)
+    result2 = cp.count_patt(br)
+    result3 = cp.count_patt(wl)
     print(result)
     print(result2)
     print(result3)
