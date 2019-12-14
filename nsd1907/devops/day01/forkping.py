2e7700beda5ecec04a191b96ef1a222ccc7d231c
MrZhangzhg <zhangzg@tedu.cn>
2019-12-14 10:27:48 +0800

    nsd1907 devops day01 10:30

---
+++ nsd1907/devops/day01/forkping.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+
+def ping(host):
+    result = subprocess.run(
+        'ping -c2 %s &> /dev/null' % host, shell=True
+    )
+    if result.returncode == 0:
+        print('%s:up' % host)
+    else:
+        print('%s:down' % host)
+
+if __name__ == '__main__':
+    ips = ['172.40.52.%s' % i for i in range(1, 255)]
+    for ip in ips:
+        retval = os.fork()
+        if not retval:
+            ping(ip)
+            exit()
