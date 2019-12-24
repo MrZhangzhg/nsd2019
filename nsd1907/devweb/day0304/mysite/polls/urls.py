8421bb2bd5477b8e106d0c4888fa2efedb09cd06
MrZhangzhg <zhangzg@tedu.cn>
2019-12-24 15:20:23 +0800

    nsd1907 devweb day03 15:21

--- nsd1907/devweb/day0304/mysite/polls/urls.py
+++ nsd1907/devweb/day0304/mysite/polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # 从http://x.x.x.x/polls/后面匹配
     # 用户访问polls首页时，将会调用views.index函数
     # 给http://x.x.x.x/polls/起名，叫index
-    url('r^$', views.index, name='index')
+    url(r'^$', views.index, name='index')
 ]
