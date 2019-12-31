c7ec294dd016586d8818ae8ad8d215335de42b06
MrZhangzhg <zhangzg@tedu.cn>
2019-12-31 11:56:05 +0800

    nsd1908 py01 day01 12:00

---
+++ nsd1908/py01/day01/hello.py
@@ -0,0 +1,17 @@
+# print('Hello World!')
+#
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+
+# a = 3; b = 4
+
+print('Hello World')  # 打印一个字符串
+# 打印两个字符串和一个数字，输出结果默认各项之间用空格分隔
+print('hao', 'ya', 123)
+# 可以通过sep指定分隔符
+print('hao', 'ya', 123, sep='***')
+# 字符串使用+进行拼接，拼接的结果输出
+print('Hello' + 'World')
+# print('hao' + 123)  # 错误，字符串与数字不能直接运算
+
