1cd32f6f927cc5af25cf6d2b694fd0334a2fb7a8
MrZhangzhg <zhangzg@tedu.cn>
2020-01-02 17:41:24 +0800

    nsd1908 py01 day02 17:40

--- nsd1908/py01/day02/for1.py
+++ nsd1908/py01/day02/for1.py
@@ -1,18 +1,24 @@
-s1 = 'abc'
-for ch in s1:
-    print(ch)
-print('*' * 30)
+# s1 = 'abc'
+# for ch in s1:
+#     print(ch)
+# print('*' * 30)
+#
+# nums = [101, 123, 2000]
+# for i in nums:
+#     print(i)
+# print('*' * 30)
+#
+# users = ('tom', 'jerry', 'bob')
+# for user in users:
+#     print(user)
+# print('*' * 30)
+#
+# adict = {'name': 'tom', 'age': 20}
+# for key in adict:
+#     print(key, adict[key])
 
-nums = [101, 123, 2000]
-for i in nums:
-    print(i)
-print('*' * 30)
+result = 0
+for i in range(1, 10000001):
+    result += i
 
-users = ('tom', 'jerry', 'bob')
-for user in users:
-    print(user)
-print('*' * 30)
-
-adict = {'name': 'tom', 'age': 20}
-for key in adict:
-    print(key, adict[key])
+print(result)
