45787c772e8e8f5f64f958c86152ebc7bcd03eec
MrZhangzhg <zhangzg@tedu.cn>
2020-01-02 10:57:41 +0800

    nsd1908 py01 day02 10:50

---
+++ nsd1908/py01/day02/grade.py
@@ -0,0 +1,12 @@
+score = int(input('分数: '))
+
+if score >= 90:
+    print('优秀')
+elif score >= 80:
+    print('好')
+elif score >= 70:
+    print('良')
+elif score >= 60:
+    print('及格')
+else:
+    print('你要努力了！！！')
