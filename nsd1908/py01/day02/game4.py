b84d396456eeb0282cd0a086de6bc1d635341cca
MrZhangzhg <zhangzg@tedu.cn>
2020-01-02 15:23:27 +0800

    nsd1908 py01 day02 15:20

---
+++ nsd1908/py01/day02/game4.py
@@ -0,0 +1,26 @@
+import random
+
+choices = ['石头', '剪刀', '布']
+# 将人胜利的情况，提前定义到列表中，人在前，计算机在后
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+prompt = """(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): """
+cwin = 0
+pwin = 0
+
+while pwin < 2 and cwin < 2:  # 人机都没有赢够2次，才继续比赛
+    computer = random.choice(choices)
+    ind = int(input(prompt))
+    player = choices[ind]
+
+    print("Your choice: %s, Computer's Choice: %s" % (player, computer))
+    if player == computer:
+        print('\033[32;1m平局\033[0m')
+    elif [player, computer] in win_list:  # 人机选项的小列表是大列表的一项
+        pwin += 1
+        print('\033[31;1mYou WIN!!!\033[0m')
+    else:
+        cwin += 1
+        print('\033[31;1mYou LOSE!!!\033[0m')
