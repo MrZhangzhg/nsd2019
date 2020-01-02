a7d01e53d7d2a01732869c0ab603b6ab38f90947
MrZhangzhg <zhangzg@tedu.cn>
2020-01-02 09:32:17 +0800

    nsd1908 py01 day02 9:30

---
+++ nsd1908/py01/day02/day02.py
@@ -0,0 +1,26 @@
+if 3 > 0:
+    print('yes')
+    print('ok')
+
+if 'to' in 'python':
+    print('true')
+else:
+    print('false')
+
+if -0.0:
+    print('任何值为0的数字都是False')
+
+if 0.001:
+    print('任何值为非0的数字都是True')
+
+if ' ':
+    print('空格也是一个字符，非空字符串为True')
+
+if []:
+    print('任何空对象都是False')
+
+if not None:
+    print('None为False，取反为True')
+
+if not 0:
+    print('数字0为False，取反为True')
