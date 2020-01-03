d099df7fa59037be1346e2a55fcbeae862fa3044
MrZhangzhg <zhangzg@tedu.cn>
2020-01-03 09:36:00 +0800

    nsd1908 py01 day03 9:40

---
+++ nsd1908/py01/day03/mtable.py
@@ -0,0 +1,10 @@
+# for i in range(3):
+#     for j in range(3):
+#         print('hello', end=' ')  # print默认在结尾打印\n，改为空格
+#     print()  # 打印回车
+
+
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('hello', end=' ')
+    print()
