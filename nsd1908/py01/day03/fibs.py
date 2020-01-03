a859834372955cd6d56533702a9478e923d22073
MrZhangzhg <zhangzg@tedu.cn>
2020-01-03 15:53:39 +0800

    nsd1908 py01 day03 15:50

--- nsd1908/py01/day03/fibs.py
+++ nsd1908/py01/day03/fibs.py
@@ -1,13 +1,15 @@
-def mk_fib():
+def mk_fib(n):
     fib = [0, 1]
 
-    n = int(input('长度: '))
     for i in range(n - 2):
         fib.append(fib[-1] + fib[-2])
 
     return fib
 
-alist = mk_fib()
-print(alist)
-blist = [i * 2 for i in alist]
-print(blist)
+for i in [5, 8, 6, 10, 20]:
+    print(mk_fib(i))
+
+# alist = mk_fib(5)
+# print(alist)
+# blist = [i * 2 for i in alist]
+# print(blist)
