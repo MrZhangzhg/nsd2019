7088b91ec976c53eaf742c9db943eda818bbf817
MrZhangzhg <zhangzg@tedu.cn>
2020-01-04 14:29:25 +0800

    nsd1908 py01 day04 14:30

--- nsd1908/py01/day04/mkfile.py
+++ nsd1908/py01/day04/mkfile.py
@@ -1,8 +1,18 @@
+import os
+
 def get_fname():
     '用于获取并返回文件名'
+    while 1:
+        fname = input('文件名: ')
+        if not os.path.exists(fname):
+            break
+        print('文件已存在，请重试')
+
+    return fname
 
 def get_content():
     '用于获取并返回文件内容'
+    
 
 def wfile(fname, content):
     '用于将内容写入文件'
