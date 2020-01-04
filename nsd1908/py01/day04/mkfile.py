79c74e4093a107a9d2aeb3e2f826198af2b892ac
MrZhangzhg <zhangzg@tedu.cn>
2020-01-04 14:42:18 +0800

    nsd1908 py01 day04 14:50

--- nsd1908/py01/day04/mkfile.py
+++ nsd1908/py01/day04/mkfile.py
@@ -12,12 +12,24 @@ def get_fname():
 
 def get_content():
     '用于获取并返回文件内容'
-    
+    content = []
+
+    print('请输入内容，在单独的一行输入end结束。')
+    while 1:
+        line = input('(end to quit)> ')
+        if line == 'end':
+            break
+        content.append(line)
+
+    return content
 
 def wfile(fname, content):
     '用于将内容写入文件'
+    with open(fname, 'w') as fobj:
+        fobj.writelines(content)
 
 if __name__ == '__main__':
     fname = get_fname()
     content = get_content()
+    content = ['%s\n' % line for line in content]
     wfile(fname, content)
