a00fe3e21fdaae193a85a4bbe15c080e79bfb350
MrZhangzhg <zhangzg@tedu.cn>
2020-01-07 10:42:14 +0800

    nsd1908 py01 day05 10:40

---
+++ nsd1908/py01/day05/stack.py
@@ -0,0 +1,16 @@
+
+def push_it():
+    '用于实现压栈功能'
+
+def pop_it():
+    '用于实现出栈功能'
+
+def view_it():
+    '用于实现查询功能'
+
+def show_menu():
+    '用于在屏幕上打印菜单，根据用户选择，调用相关功能函数'
+
+
+if __name__ == '__main__':
+    show_menu()
