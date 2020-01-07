738bf57889e077b10432f3b795a5cb8226b755a1
MrZhangzhg <zhangzg@tedu.cn>
2020-01-07 15:24:20 +0800

    nsd1908 py01 day05 15:30

---
+++ nsd1908/py01/day05/login.py
@@ -0,0 +1,29 @@
+
+def register():
+    '用于注册用户'
+    print('注册')
+
+def login():
+    '用于验证登陆'
+    print('登陆')
+
+def show_menu():
+    cmds = {'0': register, '1': login}
+    prompt = """(0) 注册
+(1) 登陆
+(2) 退出
+请选择(0/1/2): """
+    while 1:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2']:
+            print('无效的输入，请重试')
+            continue
+
+        if choice == '2':
+            print('\nBye-bye')
+            break
+
+        cmds[choice]()
+
+if __name__ == '__main__':
+    show_menu()
