17d07634ce2b353774476449e045c5ea4ba64903
MrZhangzhg <zhangzg@tedu.cn>
2020-01-08 14:14:15 +0800

    nsd1908 py02 day01 14:20

--- nsd1908/py02/day01/try1.py
+++ nsd1908/py02/day01/try1.py
@@ -1,14 +1,25 @@
+# try:
+#     num = int(input('number: '))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except ValueError:
+#     print('只接受数字')
+# except ZeroDivisionError:
+#     print('0不能作除数')
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+# except EOFError:
+#     print('\nBye-bye')
+
 try:
     num = int(input('number: '))
     result = 100 / num
     print(result)
     print('Done')
-except ValueError:
-    print('只接受数字')
-except ZeroDivisionError:
-    print('0不能作除数')
-except KeyboardInterrupt:
+except (ValueError, ZeroDivisionError) as e:  # 将异常保存到变量e中
+    print('只接受非0数字:', e)
+except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
-except EOFError:
-    print('\nBye-bye')
+
 
