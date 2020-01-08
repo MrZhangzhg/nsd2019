3310b56b1bc65d4f1eb16ab389899e52acc40c25
MrZhangzhg <zhangzg@tedu.cn>
2020-01-08 14:03:54 +0800

    nsd1908 py02 day01 14:10

--- nsd1908/py02/day01/try1.py
+++ nsd1908/py02/day01/try1.py
@@ -5,3 +5,10 @@ try:
     print('Done')
 except ValueError:
     print('只接受数字')
+except ZeroDivisionError:
+    print('0不能作除数')
+except KeyboardInterrupt:
+    print('\nBye-bye')
+except EOFError:
+    print('\nBye-bye')
+
