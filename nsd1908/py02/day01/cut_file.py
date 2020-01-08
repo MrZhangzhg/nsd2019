6e5d5d8131eb9cbd5f70835267015feb9819fbbf
MrZhangzhg <zhangzg@tedu.cn>
2020-01-08 10:45:53 +0800

    nsd1908 py02 day01 10:50

--- nsd1908/py02/day01/cut_file.py
+++ nsd1908/py02/day01/cut_file.py
@@ -8,7 +8,11 @@ t12 = time.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
 with open('weblog.txt') as fobj:
     for line in fobj:
         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
-        if t9 <= t <= t12:
+        # if t9 <= t <= t12:
+        #     print(line, end='')
+        if t > t12:
+            break
+        if t >= t9:
             print(line, end='')
 
 
