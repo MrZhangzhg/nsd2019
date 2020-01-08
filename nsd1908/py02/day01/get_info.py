ba9d4b2fa99086c4fe1e33306ffa52b733d1fb5a
MrZhangzhg <zhangzg@tedu.cn>
2020-01-08 15:22:58 +0800

    nsd1908 py02 day01 15:20

--- nsd1908/py02/day01/get_info.py
+++ nsd1908/py02/day01/get_info.py
@@ -3,3 +3,8 @@ def get_info(name, age):
         raise ValueError('年龄超过范围(1~119)')
     print('%s is %d years old' % (name, age))
 
+def get_info2(name, age):
+    # age在0到120之间什么也不会发生，如果不是，则发生AssertionError异常
+    assert 0 < age < 120, '年龄超过范围(1~119)'
+    print('%s is %d years old' % (name, age))
+
--- nsd1908/py02/day01/mytest.py
+++ nsd1908/py02/day01/mytest.py
@@ -1,6 +1,13 @@
 import get_info
 
 name = '牛老师'
-age = 200
+age = 20
+age2 = 200
 
-get_info.get_info(name, age)
+try:
+    get_info.get_info(name, age)
+    get_info.get_info2(name, age2)
+except (ValueError, AssertionError) as e:
+    print('Error:', e)
+
+
