d584d8219886d95e8bfdcb1e3a328680c48533b6
MrZhangzhg <zhangzg@tedu.cn>
2020-01-09 15:29:37 +0800

    nsd1908 py02 day02 15:30

---
+++ nsd1908/py02/day02/math_game2.py
@@ -0,0 +1,57 @@
+from random import randint, choice
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def exam():
+    '用于出题，让用户作答'
+    cmds = {'+': add, '-': sub}
+
+    # 随机生成两个数字
+    nums = [randint(1, 100) for i in range(2)]
+    # 降序排列
+    nums.sort(reverse=True)
+    # 随机选出加减法
+    op = choice('+-')
+
+    # 计算出正确答案
+    result = cmds[op](*nums)
+
+    # 要求用户作答，并判断正误
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    counter = 0
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:
+            print()   # 打印回车
+            continue
+
+        if answer == result:
+            print('Very Good!!!')
+            break
+
+        print('不对哟!!!')
+        counter += 1
+    else:
+        print('\033[31;1m正确答案: %s%s\033[0m' % (prompt, result))
+
+def main():
+    while 1:
+        exam()
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]  # 取第一个字符
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
+        if yn in 'nN':
+            print('\nBye-bye')
+            break
+
+if __name__ == '__main__':
+    main()
