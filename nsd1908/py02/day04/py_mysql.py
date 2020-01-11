a96c997c8e8f62a62bd36a1a29fa5302da63bbd5
MrZhangzhg <zhangzg@tedu.cn>
2020-01-11 14:09:46 +0800

    nsd1908 py02 day04 14:10

--- nsd1908/py02/day04/py_mysql.py
+++ nsd1908/py02/day04/py_mysql.py
@@ -11,25 +11,46 @@ conn = pymysql.connect(
 cur = conn.cursor()
 
 # 编写SQL语句
-mk_dep = """CREATE TABLE departments(
-dep_id INT, dep_name VARCHAR(50),
-PRIMARY KEY(dep_id)
-)"""
-mk_emp = """CREATE TABLE employees(
-emp_id INT, emp_name VARCHAR(20), email VARCHAR(50), dep_id INT,
-PRIMARY KEY(emp_id),
-FOREIGN KEY(dep_id) REFERENCES departments(dep_id)
-)"""
-mk_sal = """CREATE TABLE salary(
-id INT, date DATE, emp_id INT, basic INT, awards INT,
-PRIMARY KEY(id),
-FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
-)"""
+# mk_dep = """CREATE TABLE departments(
+# dep_id INT, dep_name VARCHAR(50),
+# PRIMARY KEY(dep_id)
+# )"""
+# mk_emp = """CREATE TABLE employees(
+# emp_id INT, emp_name VARCHAR(20), email VARCHAR(50), dep_id INT,
+# PRIMARY KEY(emp_id),
+# FOREIGN KEY(dep_id) REFERENCES departments(dep_id)
+# )"""
+# mk_sal = """CREATE TABLE salary(
+# id INT, date DATE, emp_id INT, basic INT, awards INT,
+# PRIMARY KEY(id),
+# FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
+# )"""
 
 # 执行SQL语句
-cur.execute(mk_dep)
-cur.execute(mk_emp)
-cur.execute(mk_sal)
+# cur.execute(mk_dep)
+# cur.execute(mk_emp)
+# cur.execute(mk_sal)
+#################################
+# 添加记录
+insert1 = 'INSERT INTO departments VALUES(%s, %s)'
+# cur.executemany(insert1, [(1, '人事部'), (2, '运维部')])
+cur.executemany(insert1, [(2, '运维部'), (3, '开发部'), (4, '财务部'), (5, '测试部'), (6, '市场部')])
+#################################
+# 修改
+# update1 = 'UPDATE departments SET dep_name=%s WHERE dep_name=%s'
+# cur.execute(update1, ('人力资源部', '人事部'))
+#################################
+# 删除
+# delete1 = 'DELETE FROM departments WHERE dep_name=%s'
+# cur.execute(delete1, ('运维部',))
+
+
+
+
+
+
+
+
 # 确认
 conn.commit()
 
