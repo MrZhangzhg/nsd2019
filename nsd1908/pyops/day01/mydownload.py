a57344af758b7f1d2c815934431d0a762b427b92
MrZhangzhg <zhangzg@tedu.cn>
2020-01-13 17:01:24 +0800

    nsd1908 devops day01 17:05

--- nsd1908/pyops/day01/mydownload.py
+++ nsd1908/pyops/day01/mydownload.py
@@ -1,15 +1,24 @@
 import wget
 import os
+import re
 
-def get_patt(fname, patt):
+def get_patt(fname, patt, charset='utf8'):
     '用于在文件中找到相关的模式'
+    result = []
+    cpatt = re.compile(patt)
+    with open(fname, encoding=charset) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:
+                result.append(m.group())
+
+    return result
 
 if __name__ == '__main__':
     url = 'http://www.163.com'
     down_dir = '/tmp/163'
     fname = '/tmp/163/163.html'
-    img_patt = ''
-    img_list = get_patt(fname, img_patt)  # 取得图片url列表
+    img_patt = '(http|https)://[\w./-]+\.(jpg|jpeg|png|gif)'
     # 下载目录不存则创建
     if not os.path.exists(down_dir):
         os.mkdir(down_dir)
@@ -17,5 +26,6 @@ if __name__ == '__main__':
     if not os.path.exists(fname):
         wget.download(url, fname)
 
+    img_list = get_patt(fname, img_patt, 'gbk')  # 取得图片url列表
     for img_url in img_list:
         wget.download(img_url, down_dir)
