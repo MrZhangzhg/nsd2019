a6409f5d73d75c6d8c8f898919cf20cfca60f803
MrZhangzhg <zhangzg@tedu.cn>
2020-01-13 11:40:52 +0800

    nsd1908 devops day01 11:40

---
+++ nsd1908/pyops/day01/zb2.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+ret_val = os.fork()
+
+if ret_val:
+    print('父进程')
+    result = os.waitpid(-1, 0)  # 挂起父进程，直到处理完子进程
+    print(result)   # (子进程pid, 0)
+    time.sleep(5)
+else:
+    print('子进程')
+    time.sleep(10)
+    print('child done')
