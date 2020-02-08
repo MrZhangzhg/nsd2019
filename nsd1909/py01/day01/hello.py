5b81185a38d9b6cc1c8580a4780c0b0727d7b574
root <root@localhost.localdomain>
2020-02-08 13:15:47 +0800

    nsd1909 py01 day01 15：00

--- nsd1909/py01/day01/hello.py
+++ nsd1909/py01/day01/hello.py
@@ -1,1 +1,20 @@
+#!/usr/local/bin/python3
+
+# print('Hello World!')
+#
+# if 3 > 10:
+#     print('yes')
+#
+# print('ok')
+
+# print可以直接打印内容
 print('Hello World!')
+
+# print也可以打印多项内容
+print('hao', 123)
+print('hao', 'ya', 123)
+
+# print打印各项时，默认使用空格分隔，也可以通过sep指定分隔符
+print('hao', 'ya', 123, sep='***')
+
+# input可以获取用户的键盘输入
