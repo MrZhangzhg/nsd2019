c6f33cc7dcd746b0160853c4b616fc5c1f1efdb2
root <root@localhost.localdomain>
2020-02-11 12:39:20 +0800

    nsd1909 py01 day03 14:30

---
+++ nsd1909/py01/day03/cp2.py
@@ -0,0 +1,17 @@
+src_fname = '/bin/ls'
+dst_fname = '/tmp/list'
+src_fobj = open(src_fname, 'rb')
+dst_fobj = open(dst_fname, 'wb')
+
+while 1:
+    data = src_fobj.read(4096)  # 从源文件中一次最多读4096字节
+
+    # if data == b''     # 如果data为空
+    # if len(data) == 0  # 如果data长度为0
+    if not data:         # data为空表示False，取反为True
+        break
+
+    dst_fobj.write(data)
+
+src_fobj.close()
+dst_fobj.close()
