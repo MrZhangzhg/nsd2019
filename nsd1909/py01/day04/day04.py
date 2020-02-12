5274b7bff93e5dff2d2b9a3b319d1e9394135891
root <root@localhost.localdomain>
2020-02-12 16:59:43 +0800

    nsd1909 py01 day04 17:40

--- nsd1909/py01/day04/day04.py
+++ nsd1909/py01/day04/day04.py
@@ -1,3 +1,11 @@
-seq = 'python'
+def shi_shuzi(s):
+    for zifu in s:
+        if not zifu in '1234567890':
+            print('不全是数字')
+            break
+    else:
+        print('全是数字')
 
-enumerate(seq)
+if __name__ == '__main__':
+    s = input('number: ')
+    shi_shuzi(s)
