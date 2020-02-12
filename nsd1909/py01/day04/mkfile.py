cccec60727000f35e585a79f5648384cb0851456
root <root@localhost.localdomain>
2020-02-12 13:34:47 +0800

    nsd1909 py01 day04 14:30

--- nsd1909/py01/day04/mkfile.py
+++ nsd1909/py01/day04/mkfile.py
@@ -1,5 +1,15 @@
+import os
+
 def get_fname():
     '用于获取文件名，返回一个不存在文件名'
+    while 1:
+        fname = input('文件名: ')
+        # os.path.exists(fname)，如果文件已存在返回True，不存在返回False
+        if not os.path.exists(fname):
+            break
+        print('文件已存在，请重试。')
+
+    return fname
 
 def get_content():
     '用于获取内容，返回一个列表'
