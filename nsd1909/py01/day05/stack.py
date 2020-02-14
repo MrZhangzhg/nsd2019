4fa3b178834699ee91d2b32dfcc08181a0523cd4
root <root@localhost.localdomain>
2020-02-14 10:58:05 +0800

    nsd1909 py01 day05 11:00

---
+++ nsd1909/py01/day05/stack.py
@@ -0,0 +1,14 @@
+def push():
+    '用于实现压栈功能'
+
+def pop():
+    '用于实现出栈功能'
+
+def view():
+    '实现查询功能'
+
+def show_menu():
+    '用于实现菜单，根据用户的选择调用相应的函数'
+
+if __name__ == '__main__':
+    show_menu()
