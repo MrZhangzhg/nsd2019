6da5a10d319bd035e665153820035e68f53a8bf8
root <root@localhost.localdomain>
2020-02-15 11:25:19 +0800

    nsd1909 py02 day01 11:25

---
+++ nsd1909/py02/day01/cut_file2.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+# 定义9点和12点的时间
+t9 = datetime.strptime('2019-05-15 09:00:00', '%Y-%m-%d %H:%M:%S')
+t12 = datetime.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
+
+# 遍历文件，时间在t9和t12之间的行，打印
+with open('mylog.log') as fobj:
+    for line in fobj:
+        t = datetime.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+        if t > t12:
+            break
+        if t >= t9:
+            print(line, end='')
