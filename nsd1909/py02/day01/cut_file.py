6d54be43b0409118ec185d98f433b37c5b30fc70
root <root@localhost.localdomain>
2020-02-15 10:45:48 +0800

    nsd1909 py02 day01 10:45

--- nsd1909/py02/day01/cut_file.py
+++ nsd1909/py02/day01/cut_file.py
@@ -8,5 +8,9 @@ t12 = time.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
 with open('mylog.log') as fobj:
     for line in fobj:
         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
-        if t9 <= t <= t12:
+        # if t9 <= t <= t12:
+        #     print(line, end='')
+        if t > t12:
+            break
+        if t >= t9:
             print(line, end='')
