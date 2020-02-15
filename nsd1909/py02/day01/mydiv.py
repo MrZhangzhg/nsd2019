b24de1c43bf4bc574a6e5b206a595bcb83ef999d
root <root@localhost.localdomain>
2020-02-15 12:24:05 +0800

    nsd1909 py02 day01 14:10

--- nsd1909/py02/day01/mydiv.py
+++ nsd1909/py02/day01/mydiv.py
@@ -1,12 +1,26 @@
+# try:
+#     n = int(input('number: '))
+#     result = 100 / n
+#     print(result)
+# except ValueError:
+#     print('输入的必须是非0数字')
+# except ZeroDivisionError:
+#     print('输入的必须是非0数字')
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+# except EOFError:
+#     print('\nBye-bye')
+#
+# print('Done')
+
 try:
     n = int(input('number: '))
     result = 100 / n
     print(result)
-except ValueError:
+except (ValueError, ZeroDivisionError):
     print('输入的必须是非0数字')
-except ZeroDivisionError:
-    print('输入的必须是非0数字')
-except KeyboardInterrupt:
+except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
-except EOFError:
-    print('\nBye-bye')
+
+print('Done')
+
