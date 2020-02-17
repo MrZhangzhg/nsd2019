a23da09f29a9628e4a09f5fbf1d19fddf50ae1d0
root <root@localhost.localdomain>
2020-02-17 12:48:55 +0800

    nsd1909 py02 day02 14:30

--- nsd1909/py02/day02/math_game.py
+++ nsd1909/py02/day02/math_game.py
@@ -1,6 +1,29 @@
+from random import randint, choice
+
 def exam():
     '用于出题，让用户作答'
-    print('exam')
+    # 随机选出两个数字
+    nums = [randint(1, 100) for i in range(2)]
+    # 降序排列
+    # nums.sort()     # 默认升序
+    # nums.reverse()  # 翻转
+    nums.sort(reverse=True)  # 上面两条的整合版本
+    # 随机选出加减法
+    op = choice('+-')
+
+    # 做出正确答案
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+
+    # 要求用户作答，并判断
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    answer = int(input(prompt))
+    if answer == result:
+        print('不错哟！！！')
+    else:
+        print('不对哟！！！')
 
 def main():
     '类似于以前的show_menu，用于展示主程序代码'
