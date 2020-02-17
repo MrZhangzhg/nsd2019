eb87078e41a6c1f1f40fa8e567272cacfb1ce5f9
root <root@localhost.localdomain>
2020-02-17 13:25:01 +0800

    nsd1909 py02 day02 15:00

--- nsd1909/py02/day02/math_game2.py
+++ nsd1909/py02/day02/math_game2.py
@@ -20,19 +20,35 @@ def exam():
     result = cmds[op](*nums)
 
     # 要求用户作答，并判断
+    counter = 0   # 计数器，用于计录用户作答次数
     prompt = '%s %s %s = ' % (nums[0], op, nums[1])
-    answer = int(input(prompt))
-    if answer == result:
-        print('不错哟！！！')
-    else:
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:   # except后面不指定具体的异常，可以捕获所有异常，不推荐
+            print()
+            continue
+
+        if answer == result:
+            print('不错哟！！！')
+            break
         print('不对哟！！！')
+        counter += 1
+    else:
+        print('%s%s' % (prompt, result))
 
 def main():
     '类似于以前的show_menu，用于展示主程序代码'
     while 1:
         exam()
         # 将用户输入的字符串去掉两端空格后，取第一个字符
-        yn = input('Continue(y/n)? ').strip()[0]
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'  # 当用户按下ctrl+c或ctrl+d时，算他输入n
+
         if yn in 'nN':
             print('\nBye-bye')
             break
