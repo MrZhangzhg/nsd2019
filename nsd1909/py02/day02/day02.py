47a9c4e1f59bc7bc0eea015998dec5eefad207e7
root <root@localhost.localdomain>
2020-02-17 14:26:38 +0800

    nsd1909 py02 day02 16:00

--- nsd1909/py02/day02/day02.py
+++ nsd1909/py02/day02/day02.py
@@ -1,13 +1,38 @@
+# from random import randint
+#
+# def func1(x):
+#     '接受一个数字，奇数返回True，偶数返回False'
+#     return True if x % 2 == 1 else False
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     result = filter(func1, nums)
+#     result2 = filter(lambda x: True if x % 2 == 1 else False, nums)
+#     print(list(result))
+#     print(list(result2))
+
+
 from random import randint
 
-def func1(x):
-    '接受一个数字，奇数返回True，偶数返回False'
-    return True if x % 2 == 1 else False
+def func2(x):
+    return x + 2
 
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    result = filter(func1, nums)
-    result2 = filter(lambda x: True if x % 2 == 1 else False, nums)
+    result = map(func2, nums)
     print(list(result))
+    result2 = map(lambda x: x + 2, nums)
     print(list(result2))
+
+
+
+
+
+
+
+
+
+
+
