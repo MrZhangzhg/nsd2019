2b22dc933a9452eec29a58c1c047ea4fa1a983f4
root <root@localhost.localdomain>
2020-02-18 15:20:31 +0800

    nsd1909 py02 day03 15:20

--- nsd1909/py02/day03/roles3.py
+++ nsd1909/py02/day03/roles3.py
@@ -1,8 +1,9 @@
 class Role:
-    def __init__(self, name, wuqi):
+    def __init__(self, name, wuqi, guojia):
         '称作构造器方法，创建实例时，自动调用。通常用于将属性绑定到实例。'
         self.name = name
         self.wuqi = wuqi
+        self.guojia = guojia
 
     def show_me(self):
         # 绑定在实例身上的属性，可以在类中的任意位置使用
@@ -14,15 +15,24 @@ class Role:
 
 class ZhanShi(Role):  # 括号中是父类，也叫基类
     # 子类将会继承父类的所有方法
-    pass
+    def __init__(self, name, wuqi, guojia, zuoji):
+        # Role.__init__(self, name, wuqi, guojia)  # 调用父类方法
+        super(ZhanShi, self).__init__(name, wuqi, guojia)  # 调用父类方法，与上一行一样
+        self.zuoji = zuoji
+
+    def attack(self, target):
+        print('正在近身与%s肉搏' % target)
 
 class FaShi(Role):
-    pass
+    def attack(self, target):
+        print('远程打击%s' % target)
 
 if __name__ == '__main__':
-    lb = ZhanShi('吕布', '方天画戟')
-    lb.show_me()
-    lb.speak('马中赤兔，人中吕布')
-    km = FaShi('孔明', '羽扇')
-    km.show_me()
-    km.speak('三分天下有其一')
+    lb = ZhanShi('吕布', '方天画戟', '汉', '赤兔马')
+    # lb.show_me()
+    # lb.speak('马中赤兔，人中吕布')
+    # lb.attack('张飞')
+    # km = FaShi('孔明', '羽扇')
+    # km.show_me()
+    # km.speak('三分天下有其一')
+    # km.attack('司马懿')
