e1b02ed0678c51cd91535161476cc14a06eeb6f5
root <root@localhost.localdomain>
2020-02-18 15:38:55 +0800

    nsd1909 py02 day03 15:40

--- nsd1909/py02/day03/myclass.py
+++ nsd1909/py02/day03/myclass.py
@@ -2,19 +2,29 @@ class A:
     def func1(self):
         print('in A func1')
 
+    def func4(self):
+        print('A func4')
+
 class B:
     def func2(self):
         print('in B func2')
 
-class C(A, B):  # 父类可以有多个
+    def func4(self):
+        print('B func4')
+
+class C(B, A):  # 父类可以有多个
     def func3(self):
         print('in C func3')
+
+    def func4(self):
+        print('C func4')
 
 if __name__ == '__main__':
     c1 = C()    # 子类实例拥有所有类的方法
     c1.func1()
     c1.func2()
     c1.func3()
+    c1.func4()
 
 
 
