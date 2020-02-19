9254646511da9c7cc194159b7c55c14b66854888
root <root@localhost.localdomain>
2020-02-19 13:05:08 +0800

    nsd1909 py02 day04 14:50

---
+++ nsd1909/py02/day04/py_mysql.py
@@ -0,0 +1,35 @@
+import pymysql
+
+# 建立连接
+conn = pymysql.connect(
+    host='127.0.0.1', port=3306,
+    user='root', passwd='tedu.cn',
+    db='nsd1909', charset='utf8'
+)
+
+# 创建游标。游标类似于文件对象，通过文件对象可以对文件读写，通过游标对数据库进行操作
+cur = conn.cursor()
+
+# 编写并执行相应的sql语句
+mk_dep = '''CREATE TABLE departments(
+dep_id INT, dep_name VARCHAR (20),
+PRIMARY KEY (dep_id)
+)'''
+mk_emp = '''CREATE TABLE employees(
+emp_id INT, emp_name VARCHAR (20), email VARCHAR (50), dep_id INT,
+PRIMARY KEY (emp_id), FOREIGN KEY (dep_id) REFERENCES departments(dep_id)
+)'''
+mk_sal = '''CREATE TABLE salary(
+id INT, date DATE, emp_id INT, basic INT, awards INT,
+PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES employees(emp_id)
+)'''
+cur.execute(mk_dep)
+cur.execute(mk_emp)
+cur.execute(mk_sal)
+
+# 确认
+conn.commit()
+
+# 关闭游标、关闭连接
+cur.close()
+conn.close()
