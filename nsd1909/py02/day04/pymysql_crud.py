3bef9f36e7d06a61d9e0fded4ffd8f6ff72ad975
root <root@localhost.localdomain>
2020-02-19 14:17:02 +0800

    nsd1909 py02 day04 16:00

--- nsd1909/py02/day04/pymysql_crud.py
+++ nsd1909/py02/day04/pymysql_crud.py
@@ -21,15 +21,23 @@ sql1 = 'INSERT INTO departments VALUES (%s, %s)'
 
 # 查询
 sql2 = 'SELECT * FROM departments ORDER BY dep_id'
-cur.execute(sql2)
-result1 = cur.fetchone()    # 取出一条记录
-print(result1)
-print('*' * 50)
-result2 = cur.fetchmany(2)  # 继续向后取出2条记录
-print(result2)
-print('*' * 50)
-result3 = cur.fetchall()    # 继续向后取出全部记录
-print(result3)
+# cur.execute(sql2)
+# result1 = cur.fetchone()    # 取出一条记录
+# print(result1)
+# print('*' * 50)
+# result2 = cur.fetchmany(2)  # 继续向后取出2条记录
+# print(result2)
+# print('*' * 50)
+# result3 = cur.fetchall()    # 继续向后取出全部记录
+# print(result3)
+
+# 修改
+sql3 = 'UPDATE departments SET dep_name=%s WHERE dep_name=%s'
+# cur.execute(sql3, ('人力资源部', '人事部'))
+
+# 删除
+sql4 = 'DELETE FROM departments WHERE dep_id=%s'
+cur.execute(sql4, (6,))  # 即使只有一项内容，第2个参数也必须是元组
 
 # 确认
 conn.commit()
