f1af52969e72c19ce2dee11f1d509d8f7095d5a8
root <root@localhost.localdomain>
2020-02-19 13:14:36 +0800

    nsd1909 py02 day04 14:55

--- nsd1909/py02/day04/py_mysql.py
+++ nsd1909/py02/day04/py_mysql.py
@@ -11,19 +11,22 @@ conn = pymysql.connect(
 cur = conn.cursor()
 
 # 编写并执行相应的sql语句
+# departments -> 部门
 mk_dep = '''CREATE TABLE departments(
 dep_id INT, dep_name VARCHAR (20),
 PRIMARY KEY (dep_id)
 )'''
+# employees -> 雇员
 mk_emp = '''CREATE TABLE employees(
 emp_id INT, emp_name VARCHAR (20), email VARCHAR (50), dep_id INT,
 PRIMARY KEY (emp_id), FOREIGN KEY (dep_id) REFERENCES departments(dep_id)
 )'''
+# salary -> 工资
 mk_sal = '''CREATE TABLE salary(
 id INT, date DATE, emp_id INT, basic INT, awards INT,
 PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES employees(emp_id)
 )'''
-cur.execute(mk_dep)
+cur.execute(mk_dep)   # execute -> 执行
 cur.execute(mk_emp)
 cur.execute(mk_sal)
 
