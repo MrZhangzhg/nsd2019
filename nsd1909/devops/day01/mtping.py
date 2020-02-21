87490843ba5f8fae6b3a3680cf1958e29cd7056b
root <root@localhost.localdomain>
2020-02-21 14:10:40 +0800

    nsd1909 devops day01 15:40

---
+++ nsd1909/devops/day01/mtping.py
@@ -0,0 +1,15 @@
+import subprocess
+import threading
+
+def ping(host):
+    result = subprocess.run('ping -c2 %s &> /dev/null' % host, shell=True)
+    if result.returncode == 0:
+        print('%s:up' % host)
+    else:
+        print('%s:down' % host)
+
+if __name__ == '__main__':
+    ips = ['192.168.113.%s' % i for i in range(1, 255)]
+    for ip in ips:
+        t = threading.Thread(target=ping, args=(ip,))
+        t.start()  # 启动工作线程，执行target(*args)
