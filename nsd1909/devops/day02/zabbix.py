54f6d2c7b348a69f13ad5f38f2fbdc8207528749
root <root@localhost.localdomain>
2020-02-22 16:36:21 +0800

    nsd1909 devops day02 18:00

--- nsd1909/devops/day02/zabbix.py
+++ nsd1909/devops/day02/zabbix.py
@@ -25,21 +25,94 @@ headers = {'Content-Type': 'application/json-rpc'}
 # }
 #####################################
 # 获取所有监控的主机
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "host.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 # "Zabbix server",
+#                 # "Linux server"
+#             ]
+#         }
+#     },
+#     "auth": "1e3972f1bf319cf35ea4bd8cc22f043b",
+#     "id": 1
+# }
+#####################################
+# 获取Linux servers组id  -> 2
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 # "Zabbix servers",
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "1e3972f1bf319cf35ea4bd8cc22f043b",
+#     "id": 1
+# }
+#####################################
+# 获取Template OS Linux模板id  -> 10001
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 # "Template OS Windows"
+#             ]
+#         }
+#     },
+#     "auth": "1e3972f1bf319cf35ea4bd8cc22f043b",
+#     "id": 1
+# }
+#####################################
+# 创建一台监控主机名为nsd1909web1，它在Linux servers组中，应用Template OS Linux模板
 data = {
     "jsonrpc": "2.0",
-    "method": "host.get",
+    "method": "host.create",
     "params": {
-        "output": "extend",
-        "filter": {
-            "host": [
-                # "Zabbix server",
-                # "Linux server"
-            ]
+        "host": "nsd1909web1",
+        "interfaces": [   # 使用哪种方法进行监控
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.113.1",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234asdfasdf",
+            "macaddress_b": "56768234aljfieri23jlf"
         }
     },
     "auth": "1e3972f1bf319cf35ea4bd8cc22f043b",
     "id": 1
 }
+
+
+
 
 #####################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
