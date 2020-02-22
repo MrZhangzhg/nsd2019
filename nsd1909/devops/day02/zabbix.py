cb726f97a2dd1cc794c7b5313202ebf35b51e6ed
root <root@localhost.localdomain>
2020-02-22 16:09:54 +0800

    nsd1909 devops day02 17:40

--- nsd1909/devops/day02/zabbix.py
+++ nsd1909/devops/day02/zabbix.py
@@ -5,14 +5,41 @@ url = 'http://192.168.113.133/zabbix/api_jsonrpc.php'
 headers = {'Content-Type': 'application/json-rpc'}
 #####################################
 # 获取zabbix版本
+# data = {
+#     "jsonrpc": "2.0",              # jsonrpc版本
+#     "method": "apiinfo.version",   # 使用的方法
+#     "params": [],                  # 参数
+#     "id": 1                        # 随便给定一个数字，表示作业号
+# }
+#####################################
+# 获取其他隐私数据，需要认证。认证时采用令牌的形式
+# 1e3972f1bf319cf35ea4bd8cc22f043b
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "user.login",
+#     "params": {
+#         "user": "Admin",
+#         "password": "zabbix"
+#     },
+#     "id": 1
+# }
+#####################################
+# 获取所有监控的主机
 data = {
-    "jsonrpc": "2.0",              # jsonrpc版本
-    "method": "apiinfo.version",   # 使用的方法
-    "params": [],                  # 参数
-    "id": 1                        # 随便给定一个数字，表示作业号
+    "jsonrpc": "2.0",
+    "method": "host.get",
+    "params": {
+        "output": "extend",
+        "filter": {
+            "host": [
+                # "Zabbix server",
+                # "Linux server"
+            ]
+        }
+    },
+    "auth": "1e3972f1bf319cf35ea4bd8cc22f043b",
+    "id": 1
 }
-
-
 
 #####################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
